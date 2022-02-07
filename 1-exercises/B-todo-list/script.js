62b8d74c60a6fd3ea73282104a1ce0946560c344
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-02-07 09:14:39 +0000

    Update script.js

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,27 @@
 function todoList(todos) {
-  // Write your code here...
-}
+  let newDiv = document.getElementById("content");
+  let newList = document.createElement("ul");
+  newDiv.appendChild(newList);
 
+  let newLi = [];
+  let newParagraph = [];
+
+  for (let i in todos) {
+    newLi[i] = document.createElement("li");
+    newParagraph[i] = document.createElement("p");
+    newParagraph[i].innerText = Object.values(todos[i]);
+    newList.appendChild(newLi[i]);
+    newLi[i].appendChild(newParagraph[i]);
+    newLi[i].addEventListener("click", () => {
+      newLi[i].style.textDecoration = "line-through";
+    });
+  }
+}
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
