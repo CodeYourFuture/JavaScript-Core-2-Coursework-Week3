a660321f6a9170e0f0efc5d1b261ec6f11110718
Harsheek Thanki <harsheekthanki@gmail.com>
2022-02-08 23:45:04 +0000

    Reading list exercise started

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,44 @@
+// Render the list of books on the page. Each book should have a title, author and image and a background colour that is set dependent on whether we have read the book or not.
+
+// ## Exercise
+
+// X Iterate through the list of books.
+// - For each book, create a `<p>` element with the book title and author.
+// - Add an `<img>` to each book that links to a URL of the book cover.
+// - Use a `<ul>` and `<li>` to display the books and append it to the page.
+// - Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
+// - All of your HTML should go inside the `<div>` with the id **"content"**.
+
+// The end result should look something like this
+
+// ![reading-list](reading-list.png)
+
 function readingList(books) {
   // Write your code here...
+  books.forEach((book) => {
+    console.log(book);
+  });
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
