14d92a2683d42858fe4cf9d0b1c073978b16422a
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-02-08 00:11:02 +0000

    Add B-todo-list

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,32 @@
 function todoList(todos) {
   // Write your code here...
+  const mainDiv = document.querySelector("#content");
+  const todoList = document.createElement("ul");
+
+  let toDoItems = todos
+    .map((todo) => {
+      return `
+    <li>${todo.todo}</li>
+    `;
+    })
+    .join("");
+
+  todoList.innerHTML = toDoItems;
+  mainDiv.append(todoList);
+
+  let items = document.querySelectorAll("li");
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("strike");
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
