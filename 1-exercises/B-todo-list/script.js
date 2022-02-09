76abe507767920c2adfb1415d8f735a7ccc3a7f8
Hachi-ops <elen20@protonmail.com>
2022-02-09 14:07:26 +0000

    exercises: B

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,27 @@
 function todoList(todos) {
   // Write your code here...
+  for (let todoEl of todos) {
+  
+  let content = document.querySelector('#content');
+  const todoList = document.createElement("ul");
+  content.appendChild(todoList);
+
+  const todoListItem = document.createElement("li");
+  todoList.appendChild(todoListItem);
+
+  const paragraph = document.createElement("p");
+  paragraph.innerText = `${todoEl.todo}`;
+  todoListItem.appendChild(paragraph);
+  paragraph.style.cursor = "pointer";  
+  
+  const clickItem = function() {
+    todoListItem.style.textDecoration === "" ?
+    todoListItem.style.textDecoration = "line-through":
+    todoListItem.style.textDecoration = "";        
+  };
+
+  todoListItem.addEventListener("click", clickItem);
+  };
 }
 
 const todos = [
