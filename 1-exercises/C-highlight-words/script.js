a865a9e3958e51267c1d8527bc6ee692438609af
Hachi-ops <elen20@protonmail.com>
2022-02-09 22:39:27 +0000

    exercises: C

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -1,7 +1,34 @@
 function highlightWords(paragraph, colours) {
-  // Write your code here...
-}
+  const content = document.querySelector("#content");
+  
+  const paragraphEl = document.createElement("p");
+  content.appendChild(paragraphEl);
+  paragraphEl.innerHTML = `${paragraph}`;
+ 
+  const selectEl = document.createElement("select");
+  content.appendChild(selectEl); 
+ 
+    for (let colour of colours) {     
+      const optionEl = document.createElement("option");
+      selectEl.appendChild(optionEl);
+      optionEl.innerHTML = colour;
+    }
 
+    let split = paragraph.split(' ');
+  split.forEach((element) => {
+    let spanEl = document.createElement('span');
+    spanEl.innerText = element + ' ';
+    paragraphEl.appendChild(spanEl);
+    spanEl.addEventListener('click', () => {
+      let selectValue = document.querySelector('select').value;
+      if (selectValue === 'none') {
+        spanEl.style.backgroundColor = '';
+      } else {
+        spanEl.style.backgroundColor = selectValue;
+      }
+    });
+  });
+}
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
 
