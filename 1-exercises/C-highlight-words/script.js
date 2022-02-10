09600ec3d4fc3a1201a7002d9c2a4fe0e99ba5b6
Hachi-ops <elen20@protonmail.com>
2022-02-10 17:47:25 +0000

    exercises: C, 2- projects: todo-list

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -14,21 +14,22 @@ function highlightWords(paragraph, colours) {
       optionEl.innerHTML = colour;
     }
 
-    let split = paragraph.split(' ');
-  split.forEach((element) => {
-    let spanEl = document.createElement('span');
-    spanEl.innerText = element + ' ';
-    paragraphEl.appendChild(spanEl);
-    spanEl.addEventListener('click', () => {
-      let selectValue = document.querySelector('select').value;
-      if (selectValue === 'none') {
-        spanEl.style.backgroundColor = '';
+    let split = paragraph.split(" ");
+  split.forEach((word) => {
+    let span = document.createElement("span");
+    span.innerText = word + " " ;
+    paragraphEl.appendChild(span);
+    span.addEventListener("click", () => {
+      let selectValue = document.querySelector("select").value;
+      if (selectValue === "none") {
+        span.style.backgroundColor = "";
       } else {
-        spanEl.style.backgroundColor = selectValue;
+        span.style.backgroundColor = selectValue;
       }
     });
   });
 }
+
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
 
--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,25 +1,49 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+
+  todos.forEach((todo) => {
+    let todoItem = document.createElement("li");
+    todoItem.innerHTML = todo.task; 
+    todoItem.classList.add("list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center");
+    let span = document.createElement("span");
+    span.classList.add("badge", 
+    "bg-primary",
+    "rounded-pill");
+    let check = document.createElement("i");
+    check.classList.add("fa", "fa-check");
+    let trash = document.createElement("i");
+    trash.classList.add("fa", "fa-trash");
+    trash.style.marginLeft = "15px";
+    span.appendChild(check);
+    span.appendChild(trash);
+    todoItem.appendChild(span);
+    list.appendChild(todoItem);
+
+        check.addEventListener("click", () => {
+      if (todoItem.style.textDecoration === "") {
+        todo.completed = true;
+        todoItem.style.textDecoration = 'line-through';
+      } else {
+        todo.completed = false;
+        todoItem.style.textDecoration = "";
+      }
+    });
+
+    trash.addEventListener("click", () => {
+      list.removeChild(todoItem);
+    });
+  });
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
+  { task: 'Wash the dishes', completed: false },
+  { task: 'Do the shopping', completed: false },
 ];
 
 populateTodoList(todos);
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
-}
-
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  // Write your code here...
-}
