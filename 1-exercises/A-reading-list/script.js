3fa5f919ab54500e55d63dd1456a6808d54227c6
Hamra <hamra.khan.b@gmail.com>
2022-02-11 15:41:20 +0000

    completed A - C

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,53 @@
+const content = document.querySelector("#content");
+
 function readingList(books) {
-  // Write your code here...
+  const unorderedList = document.createElement("ul");
+  unorderedList.classList.add("books");
+
+  books.forEach((book) => {
+    const listElement = document.createElement("li");
+    listElement.classList.add("book-description");
+
+    if (book.alreadyRead === true) {
+      listElement.classList.add("read");
+    } else {
+      listElement.classList.add("unread");
+    }
+
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("title-and-author");
+    paragraph.innerHTML = `${book.title} by ${book.author}`;
+    listElement.appendChild(paragraph);
+
+    const image = document.createElement("img");
+    image.src = book.bookCoverImage;
+    listElement.appendChild(image);
+
+    unorderedList.appendChild(listElement);
+  });
+
+  content.appendChild(unorderedList);
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,31 @@
+const content = document.querySelector("#content");
+
 function todoList(todos) {
-  // Write your code here...
+  //create ul element with class name
+  const unorderedList = document.createElement("ul");
+  unorderedList.classList.add("todo-tasks");
+  
+  todos.forEach(task => {
+    //create li element with class name
+    const listElement = document.createElement("li");
+    listElement.classList.add("task");
+    listElement.innerHTML = task.todo;
+    
+
+    //add event listener to add/remove line through text on click 
+    listElement.addEventListener('click', (e) => {
+      if (listElement.style.textDecoration === "") {
+        e.target.closest("li").style.textDecoration = "line-through";
+      } else {
+        e.target.closest("li").style.textDecoration = "";
+      }
+    }); 
+    
+    unorderedList.appendChild(listElement);
+    
+  });
+  
+  content.appendChild(unorderedList);
 }
 
 const todos = [
