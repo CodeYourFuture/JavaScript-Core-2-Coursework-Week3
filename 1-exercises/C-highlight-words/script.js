8933f3c52f8de8d835d7d0cfe9b0b9abb9bf64b8
Harsheek Thanki <harsheekthanki@gmail.com>
2022-02-11 00:09:34 +0000

    progress on C-highlight-words exercise

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -1,5 +1,36 @@
+// We want to render the paragraph on the page, along with a dropdown with the different colour options in it. When we click on a word in the paragraph we want to highlight it with the colour selected in the dropdown. If 'none' is selected in the dropdown, then no highlighting should be applied to the words, or highlighting should be removed if the clicked word is already highlighted.
+
+// - Create a `<p>` element.
+// - Create a `<select>` element.
+// - Iterate over the options array and create an `<option>` element in the `<select>` for each element.
+// - You'll need to turn the paragraph into an array to iterate over it. You can use the `.split(" ")` method here.
+// - Iterate over the array of words.
+// - For each word, create a `<span>` element and set the innerText to the word, plus a space - " ". Add this to the `<p>`.
+// - Each `<span>` should have an eventListener that will listen for clicks.
+// - When clicked, we need to check the value of the `<select>` element using the `.value` property.
+// - We can then update the `background-color` property of the `<span>` with the value of the select - remember that the value "none" is a special case and we need to be handled differently.
+// - All of your HTML should go inside the `<div>` with the id **"content"**.
+
 function highlightWords(paragraph, colours) {
   // Write your code here...
+  let p = document.createElement("p");
+  let select = document.createElement("select");
+
+  let content = document.querySelector("#content");
+
+  colours.forEach((colour) => {
+    let option = document.createElement("option");
+    select.appendChild(option);
+  });
+
+  paragraph.split(" ").forEach((word) => {
+    let span = document.createElement("span");
+
+    span.innerText = `${word} `;
+    p.appendChild(span);
+  });
+
+  content.appendChild(select);
 }
 
 const paragraph =
