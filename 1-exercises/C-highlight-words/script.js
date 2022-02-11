27dba9e0c31932d59480cfa30ed6c4d38d80caf7
mairacagri <mairacagri@gmail.com>
2022-02-11 16:16:58 +0000

    C-hightlight-words

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -1,6 +1,42 @@
 function highlightWords(paragraph, colours) {
-  // Write your code here...
+  let newDiv = document.querySelector("#content");
+  let newParagraph = document.createElement("p");
+  let newOption = document.createElement("select");
+  newDiv.appendChild(newParagraph);
+  newDiv.appendChild(newOption);
+  newParagraph.innerText = paragraph;
+
+  colours.forEach((colour) => {
+    let ourOption = document.createElement("option");
+    newOption.appendChild(ourOption);
+    ourOption.value = colour;
+    ourOption.innerText = colour;
+  });
+  
+  paragraph.split(" ").forEach((words) => {
+    let newSpan = document.createElement("span");
+    newSpan.innerText = words + " ";
+    newSpan.addEventListener("click", () => {
+      let currentColour = newOption.value;
+      newSpan.style.backgroundColor = currentColour;
+
+      if (currentColour === "none") {
+        newSpan.style.backgroundColor = "";
+      }
+
+    
+    });
+
+    newSpan.addEventListener("click", () => {});
+    newParagraph.appendChild(newSpan);
+  });
+
 }
+  
+ 
+
+
+ 
 
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
