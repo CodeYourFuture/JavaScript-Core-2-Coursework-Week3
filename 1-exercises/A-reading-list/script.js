b0550c8d8b6f7ccb29b0a753c23d2f66b3b72a66
s-shah-coder <saf.h.shah@gmail.com>
2022-02-11 14:30:29 +0000

    completed exercises

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,26 @@
 function readingList(books) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let bookList = document.createElement("ul");
+  content.appendChild(bookList);
+
+  books.forEach((book) => {
+    let list = document.createElement("li");
+    bookList.appendChild(list);
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = `${book.title}: ${book.author}`;
+    list.appendChild(paragraph);
+    let image = document.createElement("img");
+    image.src = book.bookCoverImage;
+    list.appendChild(image);
+    if (book.alreadyRead === false) {
+      list.style.backgroundColor = "red";
+    } else {
+      list.style.backgroundColor = "green";
+    }
+  });
+
+
 }
 
 const books = [
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,20 @@
 function todoList(todos) {
   // Write your code here...
+  const content = document.querySelector("#content");
+  const uList = document.createElement("ul");
+  content.appendChild.apply(uList);
+  todos.forEach((task) => {
+    let list = document.createElement("li");
+    list.innerHTML = task.todo;
+    uList.appendChild(list);
+    list.addEventListener("click", () => {
+      if (list.style.textDecoration === "line-through") {
+        list.style.textDecoration = "";
+      } else {
+        list.style.Decoration = "line-through";
+      }
+    });
+  });
 }
 
 const todos = [
