7a498fb253163ca237e15d3335fb6213cf1cef6d
Harsheek Thanki <harsheekthanki@gmail.com>
2022-02-11 00:29:59 +0000

    C-highlight-words exercise complete

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -6,7 +6,7 @@
 // - You'll need to turn the paragraph into an array to iterate over it. You can use the `.split(" ")` method here.
 // - Iterate over the array of words.
 // - For each word, create a `<span>` element and set the innerText to the word, plus a space - " ". Add this to the `<p>`.
-// - Each `<span>` should have an eventListener that will listen for clicks.
+// - Each `<span>` should have an eventListener  that will listen for clicks.
 // - When clicked, we need to check the value of the `<select>` element using the `.value` property.
 // - We can then update the `background-color` property of the `<span>` with the value of the select - remember that the value "none" is a special case and we need to be handled differently.
 // - All of your HTML should go inside the `<div>` with the id **"content"**.
@@ -20,6 +20,8 @@ function highlightWords(paragraph, colours) {
 
   colours.forEach((colour) => {
     let option = document.createElement("option");
+    option.value = colour;
+    option.innerText = colour;
     select.appendChild(option);
   });
 
@@ -27,10 +29,20 @@ function highlightWords(paragraph, colours) {
     let span = document.createElement("span");
 
     span.innerText = `${word} `;
+
+    span.addEventListener("click", () => {
+      if (select.value === "none") {
+        span.style.backgroundColor = "";
+      } else {
+        span.style.backgroundColor = select.value;
+      }
+    });
+
     p.appendChild(span);
   });
 
   content.appendChild(select);
+  content.appendChild(p);
 }
 
 const paragraph =
