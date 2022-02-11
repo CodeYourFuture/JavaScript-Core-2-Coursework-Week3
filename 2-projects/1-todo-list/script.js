75164c5cec1b78b5bc97d07c3a8fd04e680d1d16
Shadi <89422699+shadiwin@users.noreply.github.com>
2022-02-11 23:24:06 +0000

    Number 2 mostly done

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,9 +1,12 @@
+let input = document.querySelector(".form-control")
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 
   todos.forEach(obj => {
     let liTag = document.createElement("li");
+    liTag.className = "list-group-item d-flex justify-content-between align-items-center";
     liTag.innerHTML = obj.task;
     list.appendChild(liTag);
 
@@ -15,26 +18,21 @@ function populateTodoList(todos) {
     //li tags for check and trash
     let iCheckEl = document.createElement("i");
     let iTrashEl = document.createElement("i");
+    iCheckEl.setAttribute("aria-hidden", "true");
+    iTrashEl.setAttribute("aria-hidden", "true");
     iCheckEl.classList.add("fa", "fa-check");
     iTrashEl.classList.add("fa", "fa-trash");
     spanTag.appendChild(iCheckEl);
     spanTag.appendChild(iTrashEl);
 
-    // document.getElementsByClassName("fa-check").setAttribute("aria-hidden", "true");
-    // document.getElementsByClassName("fa-trash").setAttribute("aria-hidden", "true");
 
     iCheckEl.addEventListener("click", () => {
-      if (liTag.style.decoration === "") {
-        obj.completed = true;
-        liTag.style.decoration = "line-through";
-      } else {
-        obj.completed = false;
-        liTag.style.decoration = "";
-      }
-    });
+
+      liTag.classList.toggle("checked");
+    })
     iTrashEl.addEventListener("click", () => {
       //remove
-    });
+    })
   })
 }
 
@@ -54,9 +52,12 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let newObj = {};
+
+  let value = input.value;
+
+
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
