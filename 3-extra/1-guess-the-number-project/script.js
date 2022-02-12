fd5281de5ca029d8274b797562389ffa64c51c7e
MatildaAko <matildaako@gmail.com>
2022-02-12 01:30:45 +0000

    Main skeleton of the game complete, only the tries needed

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,16 +1,36 @@
-let randomNumber = Math.floor(Math.random() * 100 + 1);
+//let randomNumber;
+let result = document.querySelector(".final-output");
+function generateRandomNumber() {
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+}
+generateRandomNumber();
 
 function guessNumber() {
+  
   //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
-
+  let guess = parseInt(document.querySelector(".inputs-Values").value);
+  
+  
+console.log(guess);
+console.log(randomNumber);
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+  if (guess < 0 || guess > 100 || guess === "") {
+    result.textContent = "Please enter a number between 1 and 100";
+  }
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
+  else if (randomNumber < guess) {
+    result.textContent = guess + " is too high, try again";
+  }
 
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
-
+  else if (guess < randomNumber) {
+    result.textContent = guess + " is too low, try again";
+  }
   //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+  else if (guess == randomNumber) {
+    result.textContent = randomNumber + " is correct. You win!!!";
+  }
 }
 
 // For this task we will be making a "New Game" button function which will reset our game,
@@ -20,7 +40,12 @@ function guessNumber() {
 function newGame() {
   //Your code here
   //Reset randomNumber
+  generateRandomNumber();
+  guessNumber();
   //Reset users input field
+  let inputField = document.querySelector(".inputs-Values");
+  inputField.value = "";
+  result.textContent = "Please enter a number between 1 and 100 to start";
   //Reset tries, and triesTaken by the user
 }
 
@@ -33,3 +58,4 @@ function keyBoardEvents(e) {
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
 document.addEventListener("keypress", keyBoardEvents);
+document.querySelector(".btnNewGame").addEventListener("click", newGame);
