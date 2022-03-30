9043d98f762476375ead5a799058e74cc3fff11e
SenamiAhifon <89362897+SenamiAhifon@users.noreply.github.com>
2022-03-30 01:23:57 +0100

    reading list

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,7 +1,3 @@
-function readingList(books) {
-  // Write your code here...
-}
-
 const books = [
   {
     title: 'The Design of Everyday Things',
@@ -24,4 +20,38 @@ const books = [
   },
 ];
 
-readingList(books);
+let content = document.querySelector("#content");
+let unorderedList = document.createElement("ul");
+
+function readingList(books) {
+
+
+ books.forEach((book) => {
+
+  let paragraph = document.createElement("p");
+  paragraph.innerText = book.title + " by " + book.author; 
+
+  let bookImage = document.createElement("img");
+  bookImage.src = book.bookCoverImage;
+  bookImage.width = "200";
+  bookImage.height = "300";
+
+  let listItem = document.createElement("li");
+  if(book.alreadyRead === true){
+    listItem.className = "green"
+  }else{
+    listItem.className = "red"
+  }
+
+  listItem.appendChild(paragraph);
+  listItem.appendChild(bookImage);
+  unorderedList.appendChild(listItem);
+  content.appendChild(unorderedList);
+
+ });
+ 
+}
+ 
+
+console.log(readingList(books));
+
