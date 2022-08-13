9946a0ae32fd2de37734a80e9fda0cd13a1e9491
Anthony Mogotlane <almogotlane@gmail.com>
2022-08-13 23:42:24 +0200

    Completed A-reading-list 1-exercises

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,26 @@
 function readingList(books) {
   // Write your code here...
+  const content = document.querySelector("#content");
+  const ul = document.createElement("ul");
+  
+  books.forEach(elem => {
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+    const li = document.createElement("li");
+
+    p.appendChild(document.createTextNode(`"${elem.title}" by ${elem.author}`))
+    li.appendChild(p);
+    img.setAttribute("src", elem.bookCoverImage);
+    li.appendChild(img)
+    ul.appendChild(li)
+
+    if(elem.alreadyRead)
+      li.className = "green";
+    else
+      li.className = "red";
+  });
+
+  content.appendChild(ul);
 }
 
 const books = [
