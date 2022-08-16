ac8a95ff7ff79298c8c4a23888b6f17c406e9ea4
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-08-16 23:39:27 +0200

    ZA2 Christian Mfuke JavaScript Core 2 Coursework Week 3
    first commit

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,28 @@
 function todoList(todos) {
   // Write your code here...
+  const content = document.querySelector("#content");
+  const unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+  todos.forEach((task) => {
+    const list = document.createElement("li");
+    list.innerHTML = task.todo;
+    unorderedList.appendChild(list);
+
+    list.addEventListener("click", () => {
+      if (list.style.textDecoration === "line-through") {
+        list.style.textDecoration = "";
+      } else {
+        list.style.textDecoration = "line-through";
+      }
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
