abfe76cbaf959fb286d65587daaff5b2ac7a003d
Walter Tanaka Chifamba <63348127+chifambawalter@users.noreply.github.com>
2022-08-17 11:58:15 +0200

    za2-chifambawalter-js-core2-week3

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,30 @@
 function readingList(books) {
   // Write your code here...
+
+let ul_tag = document.createElement("ul");
+let content_tag = document.querySelector("#content");
+content_tag.appendChild(ul_tag);
+books.forEach(item => {
+  let li_tag = document.createElement("li");
+  ul_tag.appendChild(li_tag);
+  let paragraph = document.createElement("p");
+  li_tag.appendChild(paragraph);
+  let a_tag = document.createElement("a");
+  li_tag.appendChild(a_tag);
+  let img_tag = document.createElement("img");
+  a_tag.appendChild(img_tag);
+  paragraph.innerText = `${item.title} ${item.author}`;
+  img_tag.src = item.bookCoverImage;
+  a_tag.href = item.bookCoverImage;
+  item.alreadyRead ? li_tag.style.background = "green" : li_tag.style.background = "red"
+  li_tag.className = "list_item";
+  img_tag.classList.add("imgTag")
+  paragraph.addEventListener("click", () => {
+    img_tag.classList.toggle("move");
+  })
+});
+
+
 }
 
 const books = [
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,6 +1,37 @@
+const content = document.querySelector("#content");
+
+const inputTag = document.createElement("input");
+inputTag.type = "text";
+const submitBtn = document.createElement("button");
+submitBtn.innerText = "Submit"
+
+submitBtn.type = "submit"
+submitBtn.addEventListener("click", todoList)
+content.appendChild(inputTag);
+content.appendChild(submitBtn)
+const ulTag = document.createElement("ul");
+content.appendChild(ulTag);
+ulTag.className = "ul"
+
+
+
 function todoList(todos) {
   // Write your code here...
+
+
+  let newLi = document.createElement("li");
+  newLi.innerText = inputTag.value;
+  inputTag.value.replace("  ", "");
+  newLi.addEventListener("click", () => {
+    newLi.classList.toggle("checked")
+  })
+  ulTag.appendChild(newLi);
+
 }
+
+
+
+
 
 const todos = [
   { todo: "wash the dishes" },
@@ -10,3 +41,12 @@ const todos = [
 ];
 
 todoList(todos);
+
+todos.forEach(item => {
+  let liTag = document.createElement("li");
+  liTag.innerText = item.todo;
+  liTag.addEventListener("click", () => {
+    liTag.classList.toggle("checked")
+  })
+  ulTag.appendChild(liTag);
+});
