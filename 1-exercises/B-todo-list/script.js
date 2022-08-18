8a82e1bf216b5e600f9f8ac115be0e9f66d1f6dd
chezlinbenson <chezlinbenson11@gmail.com>
2022-08-18 23:47:11 +0200

    Progress on execrise B-TODO LIST

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,33 @@
 function todoList(todos) {
   // Write your code here...
+  let contentEl = document.getElementById("content");
+  let unorderedListEl = document.createElement("ul");
+  contentEl.appendChild(unorderedListEl);
+
+  todos.forEach((item) => {
+    // console.log(item.todo);
+
+    let listEl = document.createElement("li");
+    // let paraEl = document.createElement("p");
+    // listEl.appendChild(paraEl);
+    unorderedListEl.appendChild(listEl);
+    listEl.innerText = item.todo;
+  });
+}
+
+function markingList() {
+  console.log((listEl.style.textDecoration = "line-through"));
+}
+
+function markAsDone() {
+  console.log(markAsDone.addEventlistener("click", markingList));
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
