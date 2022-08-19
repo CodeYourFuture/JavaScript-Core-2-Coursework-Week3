11a0b18e37514dd51f41d34819e8c1a9ec73faf8
chandredewet <chandredewet@gmail.com>
2022-08-19 21:07:41 +0200

    Completed C with final edits to CSS and adding h1

--- 1-exercises/C-highlight-words/script.js
+++ 1-exercises/C-highlight-words/script.js
@@ -4,13 +4,26 @@ function highlightWords(paragraph, colours) {
   let displayEl = document.getElementById("content");
   let pEl = document.createElement("p");
   displayEl.appendChild(pEl);
+  let descEl = document.createElement("p");
+  displayEl.appendChild(descEl);
+  descEl.textContent =
+    "Select colour option from drop down and see what happens if you click on above words ";
   let selectEl = document.createElement("select");
-  displayEl.appendChild(selectEl);
+  descEl.appendChild(selectEl);
 
   colours.forEach((colour) => {
     let optEl = document.createElement("option");
     optEl.textContent = colour;
     optEl.value = colour;
+    if (colour === "yellow") {
+      optEl.className += " highlightYellow";
+    }
+    if (colour === "green") {
+      optEl.className += " highlightGreen";
+    }
+    if (colour === "blue") {
+      optEl.className += " highlightBlue";
+    }
     selectEl.appendChild(optEl);
   });
 
@@ -26,7 +39,31 @@ function highlightWords(paragraph, colours) {
     // - When clicked, we need to check the value of the `<select>` element using the `.value` property.
     //We can then update the `background-color` property of the `<span>` with the value of the select - remember that the value "none" is a special case and we need to be handled differently.
     let option = selectEl.value;
-        spanEl.addEventListener
+
+    if (option === "yellow") {
+      this.classList.remove("highlightGreen", "highlightBlue");
+      this.className += " highlightYellow";
+    }
+    if (option === "green") {
+      this.classList.remove("highlightYellow", "highlightBlue");
+      this.className += " highlightGreen";
+    }
+    if (option === "blue") {
+      this.classList.remove("highlightYellow", "highlightGreen");
+      this.className += " highlightBlue";
+      this.classList.remove(
+        "highlightYellow",
+        "highlightGreen",
+        "highlightBlue"
+      );
+    }
+    if (option === "none") {
+      this.classList.remove(
+        "highlightYellow",
+        "highlightGreen",
+        "highlightBlue"
+      );
+    }
   }
 }
 
