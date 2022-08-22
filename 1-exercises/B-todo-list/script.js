369f2e46fe7b6c43c0df51e227b8917f73a9230f
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2022-08-22 22:26:01 +0200

    exercise B-todo list complete

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,32 @@
 function todoList(todos) {
   // Write your code here...
+  const container = document.createElement("div");
+  container.id = "content";
+  document.body.appendChild(container);
+
+  const todoList = document.createElement("ul");
+  container.appendChild(todoList);
+
+  todos.forEach(({ todo }) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = todo;
+    todoList.appendChild(listItem);
+
+    listItem.addEventListener("click", () => {
+      if (listItem.classList.contains("toggleTodo")) {
+        return listItem.classList.remove("toggleTodo");
+      }
+
+      listItem.classList.add("toggleTodo");
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
