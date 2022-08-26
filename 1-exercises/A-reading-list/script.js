9393ccfc7a3e682eda70e85e293de710f0259b37
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2022-08-26 04:39:02 +0200

    Add files via upload

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,7 +1,47 @@
+// let displayEl = document.getElementById("content");
+
+
+// let listEl = document.createElement("UL");
+// listEl.setAttribute("id", "listEl")
+// displayEl.appendChild(listEl)
+
+
+// let itemEl = document.createElement("LI");
+
+// let text = document.createTextNode("List 1");
+// itemEl.appendChild(text)
+// document.getElementById("listEl").appendChild(itemEl);
+
+
 function readingList(books) {
   // Write your code here...
+  let display = document.querySelector('#content');
+  let UlList = document.createElement('UL')
+
+  display.appendChild(UlList);
+  books.forEach(book => {
+
+    let list = document.createElement('LI');
+    // list.style.listStyle = "none"
+
+    UlList.appendChild(list);
+    let paragraph = document.createElement('P');
+    paragraph.innerHTML = `${book.title}: ${book.author}`;
+    list.appendChild(paragraph);
+    let image = document.createElement('img');
+    image.src = book.bookCoverImage;
+    list.appendChild(image);
+    if (book.alreadyRead === false) {
+      list.style.backgroundColor = 'red';
+    } else {
+      list.style.backgroundColor = 'green';
+    }
+  });
 }
 
+
+
+// `${books.title}
 const books = [
   {
     title: 'The Design of Everyday Things',
@@ -25,3 +65,11 @@ const books = [
 ];
 
 readingList(books);
+
+// for (book in books) {
+//   const heading = document.createElement("p");
+//   const heading_text = document.createTextNode("The Design of Everyday Things");
+//   heading.appendChild(heading_text);
+//   document.body.appendChild(heading);
+//   document.body.appendChild()
+// }
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,6 +1,24 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let uList = document.createElement("UL");
+  content.appendChild(uList);
+
+  todos.forEach((task) => {
+    let list = document.createElement("li");
+    list.innerHTML = task.todo;
+    uList.appendChild(list);
+
+    list.addEventListener("click", () => {
+      if (list.style.textDecoration === "line-through") {
+        list.style.textDecoration = "";
+      } else {
+        list.style.textDecoration = "line-through";
+      }
+    });
+  });
 }
+
 
 const todos = [
   { todo: "wash the dishes" },
