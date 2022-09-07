f841484bab8a6935ec469c6b9a50bb535cf9f075
Mathias02 <matthiasm2016@gmail.com>
2022-09-07 16:19:36 +0200

    first commit

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,6 +1,28 @@
+
+const mainDiv = document.getElementById('content');
 function todoList(todos) {
   // Write your code here...
-}
+  const list = todos.map(item =>{
+   return `<ul>
+      <li class="line">${item.todo}</li>
+    </ul>`
+  }).join('');
+ mainDiv.innerHTML = list;
+ const style = document.querySelectorAll('.line');
+ style.forEach(ps =>{
+  ps.addEventListener('click', () =>{
+    if(ps.classList.contains('ligne')){
+      ps.classList.remove('ligne');
+    }else{
+      ps.classList.add('ligne');
+    }
+  })
+ });
+};
+
+
+// const style = document.querySelectorAll('.line');
+// console.log(style);
 
 const todos = [
   { todo: "wash the dishes" },
