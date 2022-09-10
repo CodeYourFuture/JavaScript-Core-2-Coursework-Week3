1e304ddb2d10c0c092a5347e825274aaa7fd4c1c
Kwanda Silekwa <46996545+kwanda9700@users.noreply.github.com>
2022-09-10 10:56:13 +0200

    update

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,47 @@
 function readingList(books) {
   // Write your code here...
+  const contentElement = document.getElementById("#content");
+  const ulElement = document.createElement("ul");
+
+  books.forEach((book) => {
+    const pElement = document.createElement("p");
+    const liElement = document.createElement("li");
+    const imgElement = document.createElement("img");
+
+    pElement.appendChild(
+      document.createTextNode(`"${book.title}" by ${book.author}`)
+    );
+    liElement.appendChild(pElement);
+    imgElement.setAttribute("src", book.bookCoverImage);
+    liElement.appendChild(imgElement);
+    ulElement.appendChild(liElement);
+
+    if (book.alreadyRead) liElement.className = "green";
+    else liElement.className = "red";
+
+    content.appendChild(ulElement);
+  });
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,32 @@
 function todoList(todos) {
   // Write your code here...
+  const h1Element = document.createElement("h1");
+  const contentElement = document.querySelector("#content");
+  const ulElement = document.createElement("ul");
+
+  h1Element.appendChild(document.createTextNode("To do List"));
+  document.querySelector("body").prepend(h1Element);
+
+  todos.forEach((toDoList) => {
+    const liElement = document.createElement("li");
+
+    liElement.appendChild(document.createTextNode(toDoList.todo));
+    ulElement.appendChild(liElement);
+  });
+  contentElement.appendChild(ulElement);
+
+  document.querySelectorAll("li").forEach((li) => {
+    li.addEventListener("click", () => {
+      li.classList.toggle("line-through");
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
