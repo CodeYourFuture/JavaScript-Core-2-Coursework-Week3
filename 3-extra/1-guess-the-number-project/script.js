bfb889bea1b27abfd3e74fa46da2f7014d578d49
Patou Malongi Mabando <patoumabando@gmail.com>
2022-09-12 21:42:27 +0200

    Extra project added

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -3,6 +3,7 @@ let randomNumber = Math.floor(Math.random() * 100 + 1);
 function guessNumber() {
   //Collect input from the user
   let guess = document.querySelector(".inputs-Values").value;
+  let submit = document.querySelector("btnGuess"); //submit
 
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
 
@@ -11,6 +12,26 @@ function guessNumber() {
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
 
   //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+  submit.addEventListener("click", addNumber);
+
+  function addNumber() {
+  if (guess === '') {
+    alert("Enter A Guess number")
+  } else { 
+    if (guess < 0 || guess > 100) {
+      alert("Please enter a number between 1 and 100")
+    } else {
+      if (guess < randomNumber) {
+        feedback.textContent = "Number is too high, try again"
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+      } else if (randomNumber < guess) {
+        feedback.textContent = "Number is too low, try again"
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+      } else {
+        feedback.textContent = "Guess is correct. You win!"
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+      }
+    }
 }
 
 // For this task we will be making a "New Game" button function which will reset our game,
