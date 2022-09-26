8e02ac27eca24bb4415dc2366b77d3dbe77a53ee
farzad-nazif <farzadnazif.nz@gmail.com>
2022-09-26 19:32:40 +0100

    1-exercises B-todo-list finished

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,30 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+  todos.forEach((todo) => {
+    let li = document.createElement("li");
+    li.innerText = `${todo.todo}`;
+    ul.appendChild(li);
+    li.addEventListener("click", () => {
+      let text = li.innerText;
+      if (text.includes("DONE")) {
+        li.innerText = `${todo.todo}`;
+        li.className = "notDoneTheme";
+      } else {
+        li.innerText = `${todo.todo}: DONE`;
+        li.className = "doneTheme";
+      }
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
