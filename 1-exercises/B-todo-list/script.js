dfebdc9d437e53101ff3ab79620c0dfa7c75d7cd
Pakize Bozkurt <pakiozihak@gmail.com>
2022-09-26 22:08:35 +0100

    Finished B

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,26 @@
 function todoList(todos) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+  let unOrderedList = document.createElement("ul");
+  content.appendChild(unOrderedList);
+  todos.forEach((item) => {
+    let list = document.createElement("li");
+    list.innerHTML = item.todo;
+    unOrderedList.appendChild(list);
+    list.addEventListener("click", () => {
+      if (list.style.textDecoration === "line-through") {
+        list.style.textDecoration = "";
+      } else {
+        list.style.textDecoration = "line-through";
+      }
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
