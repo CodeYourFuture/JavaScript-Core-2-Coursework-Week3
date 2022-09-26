228eab86bf134766714f2c4d1e6052c0e613e728
SaadiaElf <saadiaelfekak@gmail.com>
2022-09-26 14:27:35 +0100

    Solution exB

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,35 @@
 function todoList(todos) {
-  // Write your code here...
+  // Appending unordered list to the content div
+  let contentDiv = document.getElementById("content");
+  let list = document.createElement("ul");
+  contentDiv.appendChild(list);
+
+  todos.map((item) => {
+    // Creating elements
+    let listItem = document.createElement("li");
+    let todo = document.createElement("p");
+
+    // Adding elements content
+    todo.innerText = item.todo;
+
+    // Appending elements
+    list.appendChild(listItem);
+    listItem.appendChild(todo);
+
+    // Click events
+    listItem.addEventListener("click", () => {
+      listItem.className == "complete"
+        ? listItem.classList.remove("complete")
+        : listItem.classList.add("complete");
+    });
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
