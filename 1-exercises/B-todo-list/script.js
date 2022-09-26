ca126637eba5e5d6081f926cd8202aae12ed6f0e
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-26 15:57:15 +0100

    Update script.js

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,6 +1,30 @@
-function todoList(todos) {
-  // Write your code here...
+function todoList(todos)
+{
+  let content = document.getElementById("content");
+  let uList = document.createElement("ul");
+  content.appendChild(uList);
+  
+  todos.forEach(todo =>
+  {
+    let list = document.createElement("li");
+    list.textContent = todo.todo;
+    uList.appendChild(list);
+
+    list.addEventListener("click", () =>
+    {
+      if (list.style.textDecoration === "line-through")
+      {
+        list.style.textDecoration = "none";
+      }
+      
+      else
+      {
+        list.style.textDecoration = "line-through";
+      }
+    })
+  });
 }
+
 
 const todos = [
   { todo: "wash the dishes" },
