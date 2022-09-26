618d94f27d676e9dc75c54dc2468c335764c1f2a
SaadiaElf <saadiaelfekak@gmail.com>
2022-09-26 13:44:20 +0100

    Solution ex1

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,52 @@
 function readingList(books) {
-  // Write your code here...
+  // Appending unordered list to the content div
+  let contentDiv = document.getElementById("content");
+  let list = document.createElement("ul");
+  contentDiv.appendChild(list);
+
+  books.map((book) => {
+    // Creating elements
+    let listItem = document.createElement("li");
+    let titleAndAuthorP = document.createElement("p");
+    let image = document.createElement("img");
+
+    // Adding elements content
+    titleAndAuthorP.innerText = `${book.title} by ${book.author}`;
+    image.src = book.bookCoverImage;
+    image.className = "image";
+    listItem.classList.add("list-item");
+
+    // Appending elements
+    list.appendChild(listItem);
+    listItem.appendChild(titleAndAuthorP);
+    listItem.appendChild(image);
+
+    // Changing the style of the book depending on whether you have read it (green) or not (red)
+    book.alreadyRead
+      ? listItem.classList.add("bg--red")
+      : listItem.classList.add("bg--green");
+  });
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
