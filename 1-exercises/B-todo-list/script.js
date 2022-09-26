48127fa021ddd3eb9503c53999ec63b3e97a67e5
Kawa MANMI <kawamam2007@gmail.com>
2022-09-27 00:51:03 +0100

    Second  Exercise has been done

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,41 @@
 function todoList(todos) {
   // Write your code here...
+  // Write your code here...
+  const numberOfTodo = todos.length;
+  const listTodo = document.createElement("ul");
+  const content = document.getElementById("content");
+  content.appendChild(listTodo);
+  document.body.style.fontSize = "30px";
+  for (let i = 0; i < numberOfTodo; i++) {
+    let listMember = document.createElement("li");
+    let textListMember = document.createTextNode(`${todos[i].todo} `);
+    listMember.addEventListener("click", function () {
+      if (listMember.style.textDecoration != "line-through") {
+        listMember.style.textDecoration = "line-through";
+      } else {
+        listMember.style.textDecoration = "";
+      }
+      //Or
+      //  if (listMember.className != "lineThroughStyle") {
+      //    listMember.className = "lineThroughStyle";
+      //  } else {
+      //    listMember.className = "simpleStyle";
+      //  }
+    });
+    listMember.appendChild(textListMember);
+    listTodo.appendChild(listMember);
+  }
+}
+
+function myFunction() {
+  alert("YOU CLICKED ME!");
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
