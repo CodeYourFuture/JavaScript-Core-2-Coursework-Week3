17e08d2da23410be9fde52b3b91d6ed6c3f3624f
Kawa MANMI <kawamam2007@gmail.com>
2022-09-27 13:36:04 +0100

    The Extra has been done, I think so!

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,9 +1,44 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+let count = 0;
 
 function guessNumber() {
   //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
 
+  let guess = document.querySelector(".inputs-Values").value;
+  count++;
+  if (
+    guess.length === 0 ||
+    parseInt(guess) === 0 ||
+    parseInt(guess) > 100 ||
+    guess.includes(".") ||
+    eval(guess) <= 0
+  ) {
+    document.querySelector(".final-output").innerHTML =
+      "Please enter a number between 1 and 100";
+    console.log(parseInt(guess));
+  } else if (eval(guess) > randomNumber && count <= 10) {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `Try number: ${count}. Number is too high.`;
+  } else if (eval(guess) < randomNumber && count <= 10) {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `Try number: ${count}. Number is too LOW.`;
+  } else if (eval(guess) === randomNumber && count <= 10) {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `Guess is correct. You win!. By ${count} tries. The page restart in 10 seconds`;
+    setTimeout(function () {
+      document.location.reload();
+    }, 5000);
+  } else {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `You exceeds the number of tries. The game will restart after 10 seconds `;
+    setTimeout(function () {
+      document.location.reload();
+    }, 5000);
+  }
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
@@ -22,6 +57,7 @@ function newGame() {
   //Reset randomNumber
   //Reset users input field
   //Reset tries, and triesTaken by the user
+  document.location.reload(true);
 }
 
 //keyboard exception
@@ -33,3 +69,5 @@ function keyBoardEvents(e) {
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
 document.addEventListener("keypress", keyBoardEvents);
+
+document.querySelector(".btnNewGame").addEventListener("click", newGame);
