bb1badbf2f3af27acc6b226d70625abf41920d26
Kawa MANMI <kawamam2007@gmail.com>
2022-09-27 11:44:49 +0100

    The challange for 2-Project has been done

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -5,6 +5,7 @@ function populateTodoList(todos) {
     const listMember = document.createElement("li");
     listMember.className =
       "list-group-item d-flex justify-content-between align-items-center";
+    listMember.id = "list" + i;
     const textListMember = document.createTextNode(`${todos[i].task} `);
     listMember.appendChild(textListMember);
 
@@ -15,7 +16,7 @@ function populateTodoList(todos) {
     const spanItem1 = document.createElement("i");
     spanItem1.className = "fa fa-check";
     spanItem1.addEventListener("click", function () {
-      lineThrough(listMember);
+      lineThrough(listMember, i);
     });
     const spanItem2 = document.createElement("i");
     spanItem2.className = "fa fa-trash";
@@ -24,24 +25,36 @@ function populateTodoList(todos) {
     });
     mySpan.appendChild(spanItem1);
     mySpan.appendChild(spanItem2);
-
     listMember.appendChild(mySpan);
     list.appendChild(listMember);
+
+    if (todos[i].completed) {
+      listMember.style.textDecoration = "line-through";
+    }
   }
+  const removeAllCompleted = document.createElement("button");
+  removeAllCompleted.innerHTML = "Delete Completed";
+  list.appendChild(removeAllCompleted);
+  removeAllCompleted.className = "btn btn-primary mb-3";
+  removeAllCompleted.addEventListener("click", function () {
+    deleteAllCompletedTodos(todos);
+  });
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-  { task: "Wash the dishes", completed: false },
+  { task: "Wash the dishes", completed: true },
   { task: "Do the shopping", completed: false },
 ];
 
-function lineThrough(listItem) {
+function lineThrough(listItem, i) {
   if (listItem.style.textDecoration != "line-through") {
     listItem.style.textDecoration = "line-through";
+    todos[i].completed = true;
   } else {
     listItem.style.textDecoration = "";
+    todos[i].completed = false;
   }
 }
 
@@ -59,6 +72,14 @@ function addNewTodo(event) {
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
+function deleteAllCompletedTodos(listAlltodos) {
   // Write your code here...
+  listAlltodos.map((item) => {
+    if (item.completed) {
+      const itemId = document.getElementById(
+        "list" + listAlltodos.indexOf(item)
+      );
+      if (itemId != null) itemId.remove();
+    }
+  });
 }
