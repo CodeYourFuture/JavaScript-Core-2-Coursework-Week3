b19a9ac9c54b8941c97ebcdc6fa0f5e0beb75ec1
mesgna woldeab <mesgna.mehari8@gmail.com>
2022-09-27 22:56:56 +0100

    exersise a,b ,c

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,26 @@
 function readingList(books) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let unorderedList = document.createElement("ul");
+content.appendChild(unorderedList);
+
+  books.forEach((book) => {
+    let list = document.createElement("li");
+    unorderedList.appendChild(list);
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = `${book.title}: ${book.author}`;
+    list.appendChild(paragraph);
+    let image = document.createElement("img");
+    image.src = book.bookCoverImage;
+    list.appendChild(image);
+    if (book.alreadyRead === false) {
+      list.style.backgroundColor = "red";
+    } else {
+      list.style.backgroundColor = "green";
+    }
+  });
+
+
 }
 
 const books = [
@@ -24,4 +45,4 @@ const books = [
   },
 ];
 
-readingList(books);
+readingList(books)
