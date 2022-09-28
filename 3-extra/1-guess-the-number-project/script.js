ce88e9e155996fce5558c2e18d7578c2053a4c28
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2022-09-28 22:33:56 +0100

    complete extra nearly

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,14 +1,38 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
+let triesField = document.querySelector(".Tries-output");
+let numberOfTries = 7;
+triesField.innerText = `Number of Tries: ${numberOfTries}`;
+
 function guessNumber() {
   //Collect input from the user
   let guess = document.querySelector(".inputs-Values").value;
   let guessButton = document.querySelector(".btnGuess");
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
-  let outputField = document.querySelectoAll(".final-output");
+  let outputField = document.querySelector(".final-output");  
+
   guessButton.addEventListener("click", function () {
-    outputField.innerText = "none";
-    outputField.style.color = "red";
+    if (guess <= 0 || guess > 100 || guess == "") {
+      outputField.innerText = "Please enter a number between 1 and 100";
+      outputField.style.color = "red";
+    }
+    if (guess > randomNumber && guess <= 100) {
+      outputField.innerText = "Number is too high, try again";
+    }
+    if (guess < randomNumber && guess > 0) {
+      outputField.innerText = "Number is too low, try again";
+    }
+    if (guess == randomNumber) {
+      outputField.innerText = `${randomNumber} Guess is correct. You win!`;
+    }
+
+    let newGamebtn = document.querySelector(".btnNewGame");
+    newGamebtn.addEventListener("click", function () {
+      window.location.reload();
+    });
+    
+    numberOfTries--;
+    triesField.innerText = `Number of Tries: ${numberOfTries}`;
   });
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
@@ -23,6 +47,11 @@ function guessNumber() {
 // 1. Reset the values inside the body of the function
 // 2. Attach our new game button using an event listener to the .btnNewGame button
 function newGame() {
+  //this function is not called and doesnt do anything, its just plainly confusing, I have placed the refrech function inside the body of the above function and it works
+  // let newGamebtn = document.querySelector(".btnNewGame");
+  // newGamebtn.addEventListener("click", function () {
+  //   window.location.reload();
+  // });
   //Your code here
   //Reset randomNumber
   //Reset users input field
