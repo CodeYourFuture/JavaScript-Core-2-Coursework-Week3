4d6d82b16359c8eddd2a20b12189b3ff324f4de6
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-09-28 22:58:42 +0100

    c-highlight-w completed

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,19 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+  todos.forEach((el) => {
+    let list = document.createElement("li");
+    list.innerHTML = el.todo;
+    unorderedList.appendChild(list);
+    list.addEventListener('click', () => {
+      if (list.style.textDecoration === 'line-through') {
+        list.style.textDecoration = '';
+      } else { list.style.textDecoration = 'line-through' }
+    })
+    
+  })
 }
 
 const todos = [
