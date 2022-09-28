440970cc10bb5d339004722cf1d0ecf6c41abf08
Mitch Lloyd <mitchelllloyd154@gmail.com>
2022-09-28 11:07:35 +0100

    refactor: tidy up solution code

--- 2-projects/1-todo-list/solution.js
+++ 2-projects/1-todo-list/solution.js
@@ -1,54 +1,28 @@
 function toggleIsComplete(todo) {
   todo.completed = !todo.completed;
-  redrawTodo(todo);
 }
 
-function redrawTodo(chosenTodo) {
-  const li = [...document.querySelectorAll("li")].find(({ innerText }) => {
-    return innerText === chosenTodo.task;
-  });
-  if (chosenTodo.completed) {
+function redrawTodo(li, isComplete) {
+  if (isComplete) {
     li.style.textDecoration = "line-through";
   } else {
     li.style.textDecoration = null;
   }
 }
 
-function deleteTodo(todo) {
-  const li = [...document.querySelectorAll("li")].find(({ innerText }) => {
-    return innerText === todo.task;
-  });
+function deleteTodo(li) {
   document.querySelector("ul").removeChild(li);
 }
 
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
-  todos.forEach((todo) => {
-    const li = document.createElement("li");
-    const tickIcon = document.createElement("i");
-    tickIcon.classList.add("fa", "fa-check");
-    const binIcon = document.createElement("i");
-    binIcon.classList.add("fa", "fa-trash");
-    if (!tickIcon.onclick) {
-      tickIcon.addEventListener("click", () => toggleIsComplete(todo));
-    }
-    if (!binIcon.onclick) {
-      binIcon.addEventListener("click", () => deleteTodo(todo));
-    }
-    if (todo.completed) {
-      li.style.textDecoration = "line-through";
-    }
-    li.innerText = todo.task;
-    li.appendChild(tickIcon);
-    li.appendChild(binIcon);
 
+  todos.forEach((todo) => {
+    const li = createDOMListItem(todo);
     list.appendChild(li);
   });
 }
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
   { task: "Wash the dishes", completed: false },
   { task: "Do the shopping", completed: false },
@@ -56,8 +30,7 @@ let todos = [
 
 populateTodoList(todos);
 
-function appendTodo(todo) {
-  let list = document.getElementById("todo-list");
+function createDOMListItem(todo) {
   const li = document.createElement("li");
 
   const tickIcon = document.createElement("i");
@@ -66,35 +39,39 @@ function appendTodo(todo) {
   binIcon.classList.add("fa", "fa-trash");
 
   if (!tickIcon.onclick) {
-    tickIcon.addEventListener("click", () => toggleIsComplete(todo));
+    tickIcon.addEventListener("click", () => {
+      toggleIsComplete(todo);
+      redrawTodo(li, todo.completed);
+    });
   }
   if (!binIcon.onclick) {
-    binIcon.addEventListener("click", () => deleteTodo(todo));
+    binIcon.addEventListener("click", () => deleteTodo(li));
   }
   if (todo.completed) {
     li.style.textDecoration = "line-through";
   }
+
   li.innerText = todo.task;
   li.appendChild(tickIcon);
   li.appendChild(binIcon);
+  return li;
+}
+
+function appendTodo(todo) {
+  let list = document.getElementById("todo-list");
+  const li = createDOMListItem(todo);
   list.appendChild(li);
 }
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
 
   const { value: task } = document.querySelector("#todoInput");
-
   appendTodo({ task, completed: false });
 }
 
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos(e) {
   e.preventDefault();
-  // Write your code here...
   const list = document.querySelector("ul");
   const completedTodos = [...document.querySelectorAll("li")].filter((todo) => {
     return todo.style.textDecoration === "line-through";
