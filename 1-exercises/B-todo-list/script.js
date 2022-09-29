4d9de69aeb86d7348204e298976aa4544199c057
kavitappatil <patil.poem@gmail.com>
2022-09-29 19:06:38 +0100

    finished A,B,C, exercies

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,28 @@
 function todoList(todos) {
-  // Write your code here...
+  let unOrderedList = document.createElement("ul");
+  todos.forEach((object) => {
+    let list = document.createElement("li");
+    list.innerHTML = object.todo;
+    list.className = "romove";
+    unOrderedList.appendChild(list);
+    list.addEventListener("click", lineThroughStyle);
+    function lineThroughStyle() {
+      if (list.className === "remove") {
+        list.className = "add";
+      } else {
+        list.className = "remove";
+      }
+    }
+  });
+  let divEl = document.getElementById("content");
+  divEl.appendChild(unOrderedList);
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
