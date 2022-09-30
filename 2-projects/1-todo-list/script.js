6addf797703128ee519ab712d4dd3b1cad84ffbd
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-09-30 06:55:08 +0100

    completed

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,76 +1,85 @@
-let list = document.getElementById("todo-list");
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 
-while (list.hasChildNodes()) {
-  list.removeChild(list.firstChild);
+const today = new Date();
+const dd = today.getDate();
+const mm = today.getMonth() + 1;
+const yyyy = today.getFullYear();
+const dueDateInput = document.getElementById('due-date');
+
+dueDateInput.value = `${yyyy}-${mm > 9 ? mm : '0'.concat(mm)}-${dd > 9 ? dd : '0'.concat(dd)}`;
+dueDateInput.setAttribute('min', dueDateInput.value);
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+let todos = [
+  { task: 'Wash the dishes', completed: true, dueDate: tomorrow },
+  { task: 'Do the shopping', completed: false, dueDate: tomorrow },
+];
+
+function populateTodoList(todoList) {
+  const list = document.getElementById('todo-list');
+  list.innerHTML = '';
+  todoList.forEach((todo, id) => {
+    const item = document.createElement('li');
+    item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    item.style.textDecoration = todo.completed ? 'line-through' : null;
+    item.setAttribute('id', id);
+    item.innerText = todo.task;
+
+    const dueDate = document.createElement('em');
+    const countDays = Math.ceil((todo.dueDate.getTime() - new Date().getTime()) / 24 / 3600 / 1000);
+    const dueText = countDays < 1 ? 'Due Today' : countDays === 1 ? 'Due Tomorrow' : `Due in ${countDays} days`;
+    dueDate.innerText = dueText;
+
+    item.appendChild(dueDate);
+    item.appendChild(createButtons(id));
+    list.appendChild(item);
+  });
+  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
-function populateTodoList(todos) {
-  
-  //let newList = document.createElement('ul');
-  //newList.setAttribute('id', 'todo-list');
-  //newList.setAttribute('class', 'list-group');
+populateTodoList(todos);
 
-    todos.forEach((item) => {
-    let listEl = document.createElement("li");
-    listEl.className =
-      "list-group-item d-flex justify-content-between align-items-center";
-    listEl.innerHTML = item.task;
-    let span = document.createElement("span");
-    span.className = "badge bg-primary rounded-pill";
-    let iElement = document.createElement("i");
-    iElement.className = "fa fa-check";
-    iElement.setAttribute("aria-hidden", "true");
-    let iElement2 = document.createElement("i");
-    span.appendChild(iElement);
-    iElement.addEventListener("click", () => {
-      iElement.parentElement.parentElement.classList.toggle("line-through");
-    });
-    iElement2.addEventListener("click", () => {
-      iElement2.parentElement.parentElement.remove();
-    });
-    iElement2.className = "fa fa-trash";
-    iElement2.setAttribute("aria-hidden", "true");
-    span.appendChild(iElement2);
-    listEl.appendChild(span);
-    list.appendChild(listEl);
+function createButtons(id) {
+  const buttonSpan = document.createElement('span');
+  buttonSpan.classList.add('badge', 'bg-primary', 'rounded-pill');
+  const checkButton = document.createElement('i');
+  checkButton.classList.add('fa', 'fa-check');
+  checkButton.setAttribute('aria-hidden', 'true');
+  checkButton.addEventListener('click', () => {
+    document.getElementById(id).classList.toggle('strike');
   });
 
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  const trashButton = document.createElement('i');
+  trashButton.classList.add('fa', 'fa-trash');
+  trashButton.setAttribute('aria-hidden', 'true');
+  trashButton.addEventListener('click', () => {
+    todos.splice(id, 1);
+    populateTodoList(todos);
+  });
+  buttonSpan.appendChild(checkButton);
+  buttonSpan.appendChild(trashButton);
+  return buttonSpan;
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-];
-
-populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
+  todos.push({ task: document.getElementById('todoInput').value, completed: false, dueDate: new Date(document.getElementById('due-date').value) });
+  populateTodoList(todos);
+  document.getElementById('todoInput').value = '';
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
-  let input = document.querySelector(".form-control");
-  let inputValue = [
-    {
-      task: input.value,
-      completed: false,
-    },
-  ];
-
-  populateTodoList(inputValue);
-  input.value = "";
 }
 
-// Advanced challenge: Write a funcntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  // Write your code here...
-  let toDoTask = document.querySelectorAll("li");
-  toDoTask.forEach((task) => {
-    if (task.classList.contains("line-through")) {
-      task.remove();
-    }
-  })
+// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
+function deleteAllCompletedTodos(event) {
+  event.preventDefault();
+  todos = todos.filter((e) => !e.completed);
+  populateTodoList(todos);
 }
-setInterval(deleteAllCompletedTodos, 1000);
