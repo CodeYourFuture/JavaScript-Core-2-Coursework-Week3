fe0eb8a2f8f379e75d6db9e37a5c2656479b47c8
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-02 15:09:41 +0100

    first part to do done, line not working

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,28 @@
 function todoList(todos) {
   // Write your code here...
+
+  let myToDoList = document.getElementById('content');
+  let myToDos = document.createElement('ul');
+  myToDoList.appendChild(myToDos);
+
+  todos.map((item) =>{
+    let theToDo = document.createElement('li');
+    let todo = document.createElement('p');
+    todo.innerText = item.todo;
+    myToDos.appendChild(theToDo);
+    theToDo.appendChild(todo);
+  })
+
+  theToDo.addEventListener("click", () =>{
+    if (theToDo.style.textDecoration === 'line-through'){
+      theToDo.style.textDecoration = 'none'
+    }
+    else{
+      theToDo.style.textDecoration = 'line-through'
+    }
+  })
+
+  
 }
 
 const todos = [
@@ -10,3 +33,4 @@ const todos = [
 ];
 
 todoList(todos);
+
