7c41721950eb11e9010b0665e2d7cd2a57a96d29
Dawit Abraha <akdawit1@gmail.com>
2022-10-02 15:37:02 +0100

    Tried to solve the Extra task

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,11 +1,38 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
+let triesField = document.querySelector(".Tries-output");
+let numberOfTries = 7;
+triesField.innerText = `Number of Tries: ${numberOfTries}`;
+
 function guessNumber() {
   //Collect input from the user
   let guess = document.querySelector(".inputs-Values").value;
 
+  let guessButton = document.querySelector(".btnGuess");
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+let outputField = document.querySelector(".final-output");
 
+if (guess <= 0 || guess > 100 || guess == "") {
+  outputField.innerText = "Please enter a number between 1 and 100";
+  outputField.style.color = "red";
+}
+if (guess > randomNumber && guess <= 100) {
+  outputField.innerText = "Number is too high, try again";
+}
+if (guess < randomNumber && guess > 0) {
+  outputField.innerText = "Number is too low, try again";
+}
+if (guess == randomNumber) {
+  outputField.innerText = `${randomNumber} Guess is correct. You win!`;
+}
+
+let newGamebtn = document.querySelector(".btnNewGame");
+newGamebtn.addEventListener("click", function () {
+  window.location.reload();
+});
+
+numberOfTries--;
+triesField.innerText = `Number of Tries: ${numberOfTries}`;
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
 
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
