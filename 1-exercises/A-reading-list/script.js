3f64470f78e350d8b5030527c7b17a2b54ba0e8e
4maziar <53979524+8maziar@users.noreply.github.com>
2022-10-16 18:57:30 +0100

    exercise finished

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,46 @@
 function readingList(books) {
-  // Write your code here...
+  // select the content div
+  const content = document.getElementById("content");
+  books.forEach((book) => {
+    //creating elements
+    const container = document.createElement("ul");
+    const elem = document.createElement("li");
+    const title = document.createElement("p");
+    const image = document.createElement("img");
+    // adding style and value
+    image.style.width = "160px";
+    image.setAttribute("src", book.bookCoverImage);
+    title.textContent = `${book.title} by ${book.author}`;
+    elem.style.backgroundColor = book.alreadyRead ? "green" : "red";
+    // add elements to each li
+    elem.appendChild(title);
+    elem.appendChild(image);
+    // add list items to ul
+    container.appendChild(elem);
+    // add ul containing items to content div
+    content.appendChild(container);
+  });
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,7 +1,38 @@
 function highlightWords(paragraph, colours) {
-  // Write your code here...
+  // creating elements
+  const content = document.getElementById("content");
+  const paragraphEl = document.createElement("p");
+  const select = document.createElement("select");
+  // adding options to select element
+  colours.forEach((colour) => {
+    colour;
+    const option = document.createElement("option");
+    option.value = colour;
+    option.innerText = colour;
+    select.appendChild(option);
+  });
+  // creating span elements and add event listner for each
+  paragraph.split(" ").forEach((word) => {
+    const span = document.createElement("span");
+    span.innerText = word + " ";
+    span.addEventListener("click", function () {
+      const selectColor = select.value;
+      if (selectColor === "yellow") {
+        span.style.backgroundColor = selectColor;
+      } else if (selectColor === "green") {
+        span.style.backgroundColor = selectColor;
+      } else if (selectColor === "blue") {
+        span.style.backgroundColor = selectColor;
+      } else {
+        span.style.backgroundColor = "white";
+      }
+    });
+    paragraphEl.appendChild(span);
+  });
+  // adding elements to html
+  content.appendChild(select);
+  content.appendChild(paragraphEl);
 }
-
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
 
