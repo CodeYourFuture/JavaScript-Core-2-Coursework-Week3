00f4bd55ccb8df842e0f0bb81d10b507305ef3f4
Anosh Malik <anoshmalik.am@gmail.com>
2022-10-16 08:37:11 +0100

    Finished 1A - reading list - All working as per spec

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -25,3 +25,25 @@ const books = [
 ];
 
 readingList(books);
+
+const divVar = document.querySelector("#content");
+const ulTag = divVar.appendChild(document.createElement("ul"));
+
+for (let x in books) {
+    
+  const liTags = ulTag.appendChild(document.createElement("li"));
+
+  const pTags = liTags.appendChild(document.createElement("p"));  
+  pTags.textContent = `${books[x].title} - ${books[x].author}`;
+
+  const imgTags = liTags.appendChild(document.createElement("img"));
+  imgTags.setAttribute("src", `${books[x].bookCoverImage}`);
+
+  if (books[x].alreadyRead == true) {
+    // pTags.style.color = "green";
+    liTags.style.backgroundColor = "green";
+  } else { 
+        liTags.style.backgroundColor = "red";
+
+  }
+}
