0ffc98f73e86d4508960bfe191d8362559f9ed88
Miladepour <epour.milad@gmail.com>
2022-10-17 17:32:13 +0100

    A-reading-list

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,23 @@
+let parentElement = document.querySelector("#content")
 function readingList(books) {
-  // Write your code here...
+  for (let key in books) {
+    //create list
+    let ulList = document.createElement("ul");
+    let liList = document.createElement("li");
+    //title
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = `${books[key].title} by ${books[key].author} `
+    //image
+    let picture = document.createElement("IMG");
+    picture.setAttribute("src",books[key].bookCoverImage);
+    //style
+    liList.style.backgroundColor = books[key].alreadyRead ? "green" : "red";
+    //append
+    liList.appendChild(paragraph)
+    liList.appendChild(picture)
+    ulList.appendChild(liList)
+    parentElement.appendChild(ulList)
+}
 }
 
 const books = [
