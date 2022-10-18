1a3033390dff9e2859108ee92cb01e957bcb9082
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-10-18 18:13:03 +0100

    exercise A without the back ground

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -4,24 +4,36 @@ function readingList(books) {
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
-
-readingList(books);
+let mainDiv = document.querySelector("#content");
+for (let book of books) {
+  const unorderedList = document.createElement("ul");
+  const theListItems = document.createElement("li");
+  const pelement = document.createElement("p");
+  const images = document.createElement("img");
+  images.src = book.bookCoverImage;
+  pelement.textContent = `${book.title}:by ${book.author}`;
+  theListItems.appendChild(pelement);
+  theListItems.appendChild(images);
+  unorderedList.appendChild(theListItems);
+  mainDiv.appendChild(unorderedList);
+  console.log(unorderedList);
+}
