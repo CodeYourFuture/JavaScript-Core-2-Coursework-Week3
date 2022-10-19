5296a1e911faa4afc87f943185d687068e99bdf1
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-10-19 13:14:23 +0100

    exerise A

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,27 +1,52 @@
 function readingList(books) {
-  // Write your code here...
+  // Write your code here..
+  const getParentElement = document.querySelector("#content");
+  
+  const ulElement = document.createElement("ul");
+
+  for (let book of books) {
+    const liElement = document.createElement("li");
+
+    const pElement = document.createElement("p");
+    pElement.innerText = `${book.title} By ${book.author}`;
+    liElement.appendChild(pElement);
+
+    if (book.alreadyRead === true) {
+      liElement.style.backgroundColor = "green";
+    } else {
+      liElement.style.backgroundColor = "red";
+    }
+
+    ulElement.appendChild(liElement);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = book.bookCoverImage;
+    liElement.appendChild(imgElement);
+  }
+  getParentElement.appendChild(ulElement);
+  ulElement.style.width = 90 + "vw"
 }
 
-const books = [
+const AmazonBooks = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
-readingList(books);
+readingList(AmazonBooks);
