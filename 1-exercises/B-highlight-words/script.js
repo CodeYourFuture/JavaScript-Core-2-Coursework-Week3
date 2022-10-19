29368b25944a4663e4b6903c4e57b852e62816f0
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-10-19 23:51:01 +0100

    higlisht words another methods

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -2,36 +2,39 @@ function highlightWords(paragraph, colours) {
   // Write your code here...
 
   const parentElement = document.querySelector("#content");
-  const pElement = document.createElement("p");
   const selectElement = document.createElement("select");
-  colours.forEach((color) => {
-    color;
-    const option = document.createElement("option");
-    option.value = color;
-    option.innerText = color;
-    selectElement.appendChild(option);
-  });
-
+  const pElement = document.createElement("p");
   const splitedParagraph = paragraph.split(" ");
-  Array.from(splitedParagraph).forEach((word) => {
+  for (let word of splitedParagraph) {
     const spans = document.createElement("span");
+    spans.style.border = "solid" + 2 + "1px";
+    spans.style.margin = 2 + "px";
     spans.innerText = word + " ";
 
     spans.addEventListener("click", function () {
       spans.style.backgroundColor = selectElement.value;
-      if (selectElement.value === "none") {
-        spans.style.backgroundColor = "white";
-      }
+      selectElement.value === "none"
+        ? (spans.style.backgroundColor = "white")
+        : (spans.style.backgroundColor = selectElement.value);
     });
 
     pElement.appendChild(spans);
-  });
+  }
+
+  for (let color of colours.reverse()) {
+    // reason for revese method is to be chosen automatically
+    const option = document.createElement("option");
+    option.value = color;
+    option.innerText = color;
+    selectElement.appendChild(option);
+  }
+
   parentElement.append(selectElement);
   parentElement.appendChild(pElement);
 }
 
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut.";
-const colours = ["yellow", "green", "blue", "none"];
 
+const colours = ["yellow", "green", "blue", "none"];
 highlightWords(paragraph, colours);
