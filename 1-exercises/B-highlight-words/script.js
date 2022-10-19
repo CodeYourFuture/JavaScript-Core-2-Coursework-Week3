0e44146c948a99ceae5ecdcc35386116ebb7e193
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-10-19 23:33:01 +0100

    highlight-words

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -4,36 +4,34 @@ function highlightWords(paragraph, colours) {
   const parentElement = document.querySelector("#content");
   const pElement = document.createElement("p");
   const selectElement = document.createElement("select");
-  colours.forEach(color => {
+  colours.forEach((color) => {
     color;
     const option = document.createElement("option");
     option.value = color;
     option.innerText = color;
-    selectElement.appendChild(option)
-  })
+    selectElement.appendChild(option);
+  });
 
- 
   const splitedParagraph = paragraph.split(" ");
   Array.from(splitedParagraph).forEach((word) => {
     const spans = document.createElement("span");
     spans.innerText = word + " ";
-  
-    spans.addEventListener('click', function () {
-   spans.style.backgroundColor = selectElement.value 
-      if (selectElement.value === 'none') {
+
+    spans.addEventListener("click", function () {
+      spans.style.backgroundColor = selectElement.value;
+      if (selectElement.value === "none") {
         spans.style.backgroundColor = "white";
       }
     });
 
-pElement.appendChild(spans);
+    pElement.appendChild(spans);
   });
   parentElement.append(selectElement);
   parentElement.appendChild(pElement);
 }
 
-
-
-const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut.";
+const paragraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut.";
 const colours = ["yellow", "green", "blue", "none"];
 
 highlightWords(paragraph, colours);
