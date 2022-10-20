8055ed027de03b24b7d7ba011a0907959a261ffb
Doris-Siu <doris.siu.my@gmail.com>
2022-10-20 16:27:21 +0100

    Complete exercise A

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,45 @@
 function readingList(books) {
   // Write your code here...
+  let divEl = document.getElementById("content");
+  let ulEl = document.createElement("ul");
+  divEl.appendChild(ulEl);
+
+  for (let book of books) {
+    let liEl = document.createElement("li");
+    let para = document.createElement("p");
+    para.innerText = `${book.title} by ${book.author}`;
+    liEl.appendChild(para);
+    let image = document.createElement("img");
+    image.src = book.bookCoverImage;
+    liEl.appendChild(image);
+    if (book.alreadyRead) {
+      liEl.style.backgroundColor = "green";
+    } else {
+      liEl.style.backgroundColor = "red";
+    }
+    ulEl.appendChild(liEl);
+  }
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
