20f090310569bf622ab5b6004dfa8b3b0adb2d58
Rebwar <anvar.azizi21@gmail.com>
2022-10-21 23:51:59 +0100

    3-extra

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -25,11 +25,73 @@
 // }
 
 //keyboard exception
-function keyBoardEvents(e) {
-  if (e.keyCode === 13) {
-    guessNumber();
+// function keyBoardEvents(e) {
+//   if (e.keyCode === 13) {
+//     guessNumber();
+//   }
+// }
+
+// document.querySelector(".btnGuess").addEventListener("click", guessNumber);
+// document.addEventListener("keypress", keyBoardEvents);
+//💫💫💫
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let score = 7;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(
+    ".Tries-output"
+  ).textContent = `Number of Tries: ${message}`;
+};
+function guessNumber() {
+  let guess = document.querySelector(".inputs-Values").value;
+
+  console.log(guess, typeof guess);
+
+  if (!guess || +guess > 100 || +guess < 0) {
+    document.querySelector(".final-output").textContent =
+      "Please enter a number between 1 and 100";
+  } else if (+guess === randomNumber) {
+    document.querySelector(
+      ".final-output "
+    ).textContent = `  ${randomNumber} is correct, You Win!!!`;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    if (score > highscore) {
+      highscore = score;
+      displayMessage(highscore);
+    }
+  } else if (+guess !== randomNumber) {
+    if (score > 1) {
+      document.querySelector(".final-output").textContent =
+        +guess > randomNumber
+          ? "Number is too hight, try again"
+          : "Number is too low, try again";
+      score--;
+      displayMessage(score);
+    } else {
+      displayMessage(0);
+      document.querySelector(
+        ".final-output"
+      ).textContent = `You Lose, the number was ${randomNumber}`;
+      document.querySelector(".Tries-output").textContent = "";
+    }
   }
+}
+//💫💫💫💫💫💫💫💫💫
+
+function newGame() {
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+  document.querySelector(".inputs-Values").value = " ";
+  score = 7;
+  document.querySelector(
+    ".Tries-output"
+  ).textContent = `Number Of Tries : ${score}`;
+  document.querySelector(".final-output").textContent =
+    "Get a number between 1 and 100";
 }
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
+document.querySelector(".btnNewGame").addEventListener("click", newGame);
 document.addEventListener("keypress", keyBoardEvents);
