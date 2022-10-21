caa0db6b675c7e7913caa8c3a6b79a8f2bb698f7
Cuneyt Turker <cuneytturker35@gmail.com>
2022-10-21 21:22:41 +0100

    Mandatory to do list cmpl- except advanced task

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,22 +1,61 @@
+
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  list.innerHTML = ""
+  for(let i in todos){
+    let li = `
+    <li
+      class="list-group-item d-flex justify-content-between align-items-center" id="${i}"
+    >
+      ${todos[i].task}
+      <span class="badge bg-primary rounded-pill">
+        <i class="fa fa-check" aria-hidden="true" onclick="line(this)"></i>
+        <i class="fa fa-trash" aria-hidden="true" onclick="removing(this)"></i>
+      </span>
+    </li>
+`
+list.insertAdjacentHTML("beforeend", li)
+  }
+  
 }
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
+  // { task: "Wash the dishes", completed: false },
+  // { task: "Do the shopping", completed: false },
 ];
 
 populateTodoList(todos);
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  function addNewTodo (event){
+    
+    let text = document.querySelector("#todoInput")
+    todos.push({task : text.value, completed:false});
+    text.value = "";
+    populateTodoList(todos)
+    event.preventDefault();
+  }
+
+
+function line(event){
+  
+let label = event.parentElement.parentElement
+
+  if(label.classList.contains("lines")){
+    label.classList.remove("lines")
+
+  }else{
+    label.classList.add("lines")
+  }
+  event.preventDefault()
+  
+}
+
+function removing(element){
+
+  let x = element.parentElement.parentElement.getAttribute("id")
+  todos.splice(x,1)
+  populateTodoList(todos)
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
