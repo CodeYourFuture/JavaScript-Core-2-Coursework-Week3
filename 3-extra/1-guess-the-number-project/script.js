a1131815d5e1e6ff0d62bb171ca5c54ad226e928
luan-murilo-de-oliveira-e-souza <luan.oliveiraesouza@gmail.com>
2022-10-21 17:41:28 +0100

    answer extra

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,16 +1,37 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 
+let messageGuess = document.querySelector(".final-output");
+let triesGuess = document.querySelector(".Tries-output");
+let countTries = 7;
+
 function guessNumber() {
   //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
-
-  //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+  let guessNumber = document.querySelector(".inputs-Values").value;
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
+  if (guessNumber > randomNumber && guessNumber <= 100) {
+    messageGuess.innerText = "Number is too high, try again";
+    //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
+  } else if (guessNumber < randomNumber && guessNumber > 0) {
+    messageGuess.innerText = "Number is too low, try again";
+    //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+  } else if (guessNumber == randomNumber) {
+    messageGuess.innerText = "Guess is correct. You win!";
+    //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+  } else {
+    messageGuess.innerText = "Please enter a number between 1 and 100";
+  }
 
-  //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
-
-  //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+  if (
+    countTries > 1 &&
+    messageGuess.innerText != "Please enter a number between 1 and 100"
+  ) {
+    countTries--;
+    triesGuess.innerHTML = `Number of Tries: ${countTries}`;
+  } else if (countTries === 0) {
+    triesGuess.innerHTML = `You Lose, the number was ${randomNumber}`;
+    messageGuess.innerHTML = "";
+  }
 }
 
 // For this task we will be making a "New Game" button function which will reset our game,
@@ -20,8 +41,13 @@ function guessNumber() {
 function newGame() {
   //Your code here
   //Reset randomNumber
+  randomNumber = Math.floor(Math.random() * 100 + 1);
   //Reset users input field
+  document.querySelector(".inputs-Values").value = "";
+  messageGuess.innerHTML = "Guess a number between 1 and 100";
   //Reset tries, and triesTaken by the user
+  countTries = 7;
+  triesGuess.innerHTML = `Number of Tries: ${countTries}`;
 }
 
 //keyboard exception
@@ -32,4 +58,5 @@ function keyBoardEvents(e) {
 }
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
+document.querySelector(".btnNewGame").addEventListener("click", newGame);
 document.addEventListener("keypress", keyBoardEvents);
