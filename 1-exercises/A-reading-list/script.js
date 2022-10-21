af689426fb917f758f97af96586cc2657c7e87d8
levilu800b <levilu800b@gmail.com>
2022-10-21 16:57:58 +0100

    two solutions

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -7,24 +7,47 @@ function readingList(books) {
   // Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
   // All of your HTML should go inside the `<div>` with the id **"content"**.
 
-  let content = document.querySelector("#content");
-  let ul = document.createElement("ul");
-  content.appendChild(ul);
-  books.forEach((book) => {
-    let li = document.createElement("li");
-    let p = document.createElement("p");
-    let img = document.createElement("img");
-    p.innerText = `${book.title} - ${book.author}`;
-    img.src = book.bookCoverImage;
+  // Solution using forEach
+
+  // let content = document.querySelector("#content");
+  // let ul = document.createElement("ul");
+  // content.appendChild(ul);
+  // books.forEach((book) => {
+  //   let li = document.createElement("li");
+  //   let p = document.createElement("p");
+  //   let img = document.createElement("img");
+  //   p.innerText = `${book.title} - ${book.author}`;
+  //   img.src = book.bookCoverImage;
+  //   li.appendChild(p);
+  //   li.appendChild(img);
+  //   ul.appendChild(li);
+  //   if (book.alreadyRead) {
+  //     li.style.backgroundColor = "green";
+  //   } else {
+  //     li.style.backgroundColor = "red";
+  //   }
+
+  // Solution using For loop
+  
+  const content = document.querySelector("#content");
+  const ul = document.createElement("ul");
+  content.appendChild(ul);  
+  // using for loop to iterate through the array
+  for (let i = 0; i < books.length; i++) {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+    p.innerText = `${books[i].title} - ${books[i].author}`;
+    img.src = books[i].bookCoverImage;
     li.appendChild(p);
     li.appendChild(img);
     ul.appendChild(li);
-    if (book.alreadyRead) {
+    if (books[i].alreadyRead) {
       li.style.backgroundColor = "green";
     } else {
       li.style.backgroundColor = "red";
     }
-  });
+  };
 
   // Bonus: create a button on each book's display to remove the book from the list.
 
