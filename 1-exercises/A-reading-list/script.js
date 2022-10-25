40fd6a9ed26781ec1f7081d3360da7a71c47deed
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-10-25 10:02:49 +0100

    exercises completed

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,15 +1,24 @@
 function readingList(books) {
   // Write your code here...
-// for(let i = 0; i < books.length; i++){
-//   books[i].readingList;
-// }
+
 const content = document.querySelector('#content');
-const newParagraph = document.createElement('p');
-const newImage = document.createElement('img');
-const newList = document.createElement('list');
-
-
-
+const ul = document.createElement('ul');
+content.appendChild(ul);
+for(const i = 0; i < books.length; i++) {
+    const list = document.createElement("li");
+    ul.appendChild(list);
+    const newParagraph = document.createElement('p');
+    newParagraph.innerText = `${books[i].title} - ${books[i].author}`;
+    list.appendChild(newParagraph);
+    const image = document.createElement("img");
+    image.src = books[i].bookCoverImage;
+    list.appendChild(image);
+    if (books[i].alreadyRead === true) {
+      list.style.backgroundColor = "green";
+    } else {
+      list.style.backgroundColor = "red";
+    }
+}
 }
 
 const books = [
