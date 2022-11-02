7fa0d7b861e903785c1a6388cf7ec807792baa19
AlessandraSousa <violetanomar@gmail.com>
2022-11-02 06:40:18 +0000

    initial commit

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,32 @@
 function readingList(books) {
   // Write your code here...
+  let contentDiv = document.getElementById("content");
+  let list = document.createElement("ul");
+  contentDiv.appendChild(list);
+
+  books.map((book) => {
+    // Creating elements
+    let listItem = document.createElement("li");
+    let titleAndAuthorP = document.createElement("p");
+    let image = document.createElement("img");
+
+    // Adding elements content
+    titleAndAuthorP.innerText = `${book.title} by ${book.author}`;
+    image.src = book.bookCoverImage;
+    image.className = "image";
+    listItem.classList.add("list-item");
+
+    // Appending elements
+    list.appendChild(listItem);
+    listItem.appendChild(titleAndAuthorP);
+    listItem.appendChild(image);
+
+    // Changing the style of the book depending on whether you have read it (green) or not (red)
+    book.alreadyRead
+      ? listItem.classList.add("bg--red")
+      : listItem.classList.add("bg--green");
+  });
+  //End of code
 }
 
 const books = [
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,5 +1,29 @@
 function todoList(todos) {
   // Write your code here...
+  let contentDiv = document.getElementById("content");
+  let list = document.createElement("ul");
+  contentDiv.appendChild(list);
+
+  todos.map((item) => {
+    // Creating elements
+    let listItem = document.createElement("li");
+    let todo = document.createElement("p");
+
+    // Adding elements content
+    todo.innerText = item.todo;
+
+    // Appending elements
+    list.appendChild(listItem);
+    listItem.appendChild(todo);
+
+    // Clicking events
+    listItem.addEventListener("click", () => {
+      listItem.className == "complete"
+        ? listItem.classList.remove("complete")
+        : listItem.classList.add("complete");
+    });
+  });
+  //end of code
 }
 
 const todos = [
