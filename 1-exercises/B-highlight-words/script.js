daf4d8b0d9a0cce493bfaf517ab19f456a2a8b65
MacOS <macos@blessing.home>
2022-12-30 22:54:22 +0000

    feat: adding solution to B-highlight words

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,5 +1,52 @@
 function highlightWords(paragraph, colours) {
-  // Write your code here...
+// Write your code here...
+// - Create a `<p>` element.
+// - Create a `<select>` element.
+  let p = document.createElement('p')
+  let select = document.createElement('select')
+  let content = document.getElementById('content')
+
+// - Iterate over the options array and create an `<option>` element in the `<select>` for each element.
+for (let color of colours){
+  let option = document.createElement('option')
+  option.innerText = color
+  select.appendChild(option)
+}
+// You'll need to turn the paragraph into an array to iterate over it. 
+// You can use the `.split(" ")` method here.
+let splittedWordParagraph = paragraph.split(' ')
+
+// - Iterate over the array of words.
+// - For each word, create a `<span>` element and set the innerText to the word, 
+// plus a space - " ". Add this to the `<p>`.
+
+// - Each `<span>` should have an eventListener that will listen for clicks.
+// - When clicked, we need to check the value of the `<select>` element using the `.value` property.
+// - We can then update the `background-color` property of the `<span>` with the value of the select - 
+//   remember that the value "none" is a special case and we need to be handled differently.
+
+for (let word of splittedWordParagraph){
+  let highligtWord = document.createElement('span')
+ 
+  highligtWord.addEventListener("click", changeColor)
+  function changeColor(event){
+  let currentColor = document.querySelector("select").value
+
+  // If 'none' is selected in the dropdown, then no highlighting should be applied to the words, 
+  // or highlighting should be removed if the clicked word is already highlighted.
+
+  if(currentColor == 'none') {
+    event.target.style.backgroundColor = 'white'
+  }
+  else {event.target.style.backgroundColor = currentColor}
+}
+  highligtWord.innerText = word + " "
+  p.appendChild(highligtWord)
+}
+
+// - All of your HTML should go inside the `<div>` with the id **"content"**.
+ content.appendChild(p)
+ content.appendChild(select)
 }
 
 const paragraph =
