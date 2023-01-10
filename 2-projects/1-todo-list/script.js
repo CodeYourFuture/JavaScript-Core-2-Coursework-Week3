0f15f2097f15695c62a3d179944f85f7634301a9
MacOS <macos@blessing.home>
2023-01-10 21:19:11 +0000

    feat: adding solution for to-do list

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,26 +1,52 @@
+const todoInput = document.getElementById("todoInput");
+const todoButton = document.getElementById('todoButton')
+const removeAllButton = document.getElementById('remove-all-completed')
+const todoList = document.getElementById("todo-list");
+
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, 
   // all todos should display inside the "todo-list" element.
+  for ( let item of todos){
+  const div = document.createElement('div')
+  div.classList.add('todo')
+
+  const li = document.createElement('li')
+  li.innerText = item.task;
+  li.classList.add('todo-item')
+  div.appendChild(li)
+
+  const completeButton = document.createElement('button')
+  completeButton.innerHTML = '<i class = "fa fa-check"></i>'
+  completeButton.classList.add('complete-button')
+  div.appendChild(completeButton)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerHTML = '<i class = "fa fa-trash"></i>'
+  deleteButton.classList.add('delete-button')
+  div.appendChild(deleteButton)
+  
+  todoList.appendChild(div)
+
+  todoInput.value = "";
+  }
   
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
-// let todos = [
-//   { task: "Wash the dishes", completed: false },
-//   { task: "Do the shopping", completed: false },
-// ];
+let todos = [
+  { task: "Wash the dishes", completed: true },
+  { task: "Do the shopping", completed: false },
+];
 
-// populateTodoList(todos); 
+populateTodoList(todos); 
 
 // This function will take the value of the input field and add it 
 // as a new todo to the bottom of the todo list. These new todos will need the 
 // completed and delete buttons adding like normal.
-const todoInput = document.getElementById("todoInput");
-const todoButton = document.getElementById('todoButton')
-const removeAllButton = document.getElementById('remove-all-completed')
-const todoList = document.getElementById("todo-list");
+
 
 
 todoButton.addEventListener("click", addNewTodo)
@@ -33,6 +59,7 @@ function addNewTodo(event) {
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
   const div = document.createElement('div')
   div.classList.add('todo')
+
   const li = document.createElement('li')
   li.innerText = todoInput.value;
   li.classList.add('todo-item')
@@ -64,6 +91,7 @@ function deleteCheck(e) {
   if (item.classList[0] === 'complete-button'){
     const todo = item.parentElement;
     todo.style.textDecoration = "line-through"
+    todo.classList.add('completed')
   }
 }
 
@@ -71,10 +99,11 @@ function deleteCheck(e) {
 // (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos(e) {
   // Write your code here...
-  const item = e.target;
-  const todo = item; 
-    if(todo.style.textDecoration === "line-through"){
-      todo.remove()
-    }
+  e.preventDefault();
+  let completed = document.getElementsByClassName('completed')
+  let completedArray = Array.from(completed)
+  for (let item of completedArray) {
+    item.remove();
+}
   }
 
