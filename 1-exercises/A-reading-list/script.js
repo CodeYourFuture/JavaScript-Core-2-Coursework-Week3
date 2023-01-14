e01611c35e547f3ec78a852cebeccac454f37868
nataliizab <nataliia.zab@gmail.com>
2023-01-14 22:07:04 +0000

    Complete 1-exercise/A/

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,26 @@
-function readingList(books) {
-  // Write your code here...
+function readingList() {
+  // - Change the style of the book depending on whether you have read it (green) or not (red).
+  // Hint: try adding some CSS classes to styles.css for these styles.
+  const myNewList = document.querySelector("#reading-list"); //going inside of ul list
+  for (let book of books) {
+    // const goInsideDiv = document.querySelector("#content"); //place to make changes(div)
+
+    const newList = document.createElement("li"); //creating a list
+    const myNewP = document.createElement("p"); //added text of the paragraph
+    const addImg = document.createElement("img"); // create img
+
+    myNewP.innerText = book.title + " by " + book.author; //added text of the paragraph
+    addImg.src = book.bookCoverImage; //img src
+    if (book.alreadyRead) {
+      newList.style.background = "green";
+    } else {
+      newList.classList.add("red");
+    }
+
+    newList.appendChild(myNewP); //added text of the paragraph
+    newList.appendChild(addImg); //img
+    myNewList.appendChild(newList); //adding li to ul
+  }
 }
 
 // for the tests, do not modify this array of books
