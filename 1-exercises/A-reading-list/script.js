445f66e61e9cca78e076893d7299110f6b5cac26
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-16 15:24:39 +0000

    tarted exercise 1A

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,4 +1,28 @@
+// ## Exercise
+
+// - Iterate through the list of books.
+// - For each book, create a `<p>` element with the book title and author.
+// - Add an `<img>` to each book that links to a URL of the book cover.
+// - Use a `<ul>` and `<li>` to display the books and append it to the page.
+// - Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
+// - All of your HTML should go inside the `<div>` with the id **"content"**.
+
+
 function readingList(books) {
+  const readingList = document.querySelector("#reading-list")
+
+  books.forEach((book) => {
+    const liElement = document.createElement("li");
+    const titleElement = document.createElement("p");
+    const authorElement = document.createElement("p");
+    const imgElement = document.createElement("img");
+
+    titleElement.innerText = book.title;
+    authorElement.innerText = book.author;
+
+    liElement.appendChild(titleElement);
+    liElement.appendChild(authorElement)
+  })
   // Write your code here...
 }
 
