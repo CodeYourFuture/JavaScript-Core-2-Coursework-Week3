033ef8c3d2af91291c41c1d66eeaf6c88f9669bc
Andriana <andrianasaffo@gmail.com>
2023-01-17 16:15:30 +0000

    .

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -49,7 +49,7 @@ populateTodoList(todos);
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
+
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 
   let todo = document.querySelector(".btn");
@@ -58,6 +58,7 @@ function addNewTodo(event) {
   let addTodo = document.querySelector("#addTodo");
 
   addTodo.addEventListener("click", (event) => {
+    event.preventDefault();
     const taskInput = document.querySelector("#todoInput").value;
 
     liElement.innerText = taskInput;
@@ -65,6 +66,7 @@ function addNewTodo(event) {
     list.appendChild(liElement);
   });
 }
+// addNewTodo();
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
