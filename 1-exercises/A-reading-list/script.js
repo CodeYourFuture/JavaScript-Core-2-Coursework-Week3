15355cf13a77e38548912906e34cb1d56450fcc1
Inna Poliakova <polyakova1081@gmail.com>
2023-01-18 17:17:13 +0000

    Exercise A

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,6 +1,36 @@
 function readingList(books) {
   // Write your code here...
+
+const readingList = document.querySelector('#reading-list')
+
+books.forEach((book) => {
+
+  const li = document.createElement("li");
+
+  const titleP = document.createElement('p');
+  const authorP = document.createElement('p');
+  const imgElement = document.createElement('img')
+
+  titleP.innerText = book.title;
+authorP.innerText = book.author;
+imgElement.src = book.bookCoverImage;
+
+  li.appendChild(titleP);
+  li.appendChild(authorP);
+  li.appendChild(imgElement);
+
+if (book.alreadyRead === true) {
+  li.style.backgroundColor = 'Green'
+} else {
+  li.style.backgroundColor = 'Red'
 }
+
+  readingList.appendChild(li)
+  
+})
+
+}
+
 
 // for the tests, do not modify this array of books
 const books = [
@@ -26,3 +56,25 @@ const books = [
 ];
 
 readingList(books);
+
+// function logBooks() {
+//   for(let book of books) {
+//     if (book.alreadyRead === true) {
+//       console.log(`You've already read ${book.title} by ${book.author}`)
+//     } else {
+//       console.log(`You still need to read ${book.title} by ${book.author}`)
+//     }
+//   }
+// }
+
+// const addTextBtn = document.querySelector('#addArticleBtn');
+
+// addTextBtn.addEventListener('click', () => {
+//   const text = document.querySelector("#addArticleInput").value;
+
+//   const pElement = document.createElement('p');
+//   pElement.innerText = text;
+
+//   const parent = document.querySelector('#addArticle');
+//   parent.appendChild(pElement);
+// });
