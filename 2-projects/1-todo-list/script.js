296692e18bdeb9d675ff110838da495ca596e526
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-01-18 19:50:15 +0000

    Exercise and extra and projects

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,25 +1,87 @@
-function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+function toggleIsComplete(todo) {
+    todo.completed = !todo.completed;
+}
+
+function greenrawTodo(li, isComplete) {
+    if (isComplete) {
+        li.style.textDecoration = "line-through";
+        li.style.color = "green";
+    } else {
+        li.style.textDecoration = null;
+        li.style.color = "white";
+    }
+}
+
+function deleteTodo(li) {
+    document.querySelector("ol").removeChild(li);
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-];
+function createDOMListItem(todo) {
+
+    const li = document.createElement("li");
+
+    const tickIcon = document.createElement("i");
+    tickIcon.classList.add("fa", "fa-check");
+
+    const binIcon = document.createElement("i");
+    binIcon.classList.add("fa", "fa-trash");
+
+    if (!tickIcon.onclick) {
+        tickIcon.addEventListener("click", () => {
+            toggleIsComplete(todo);
+            greenrawTodo(li, todo.completed);
+        });
+    }
+    if (!binIcon.onclick) {
+        binIcon.addEventListener("click", () => deleteTodo(li));
+    }
+    if (todo.completed) {
+        li.style.textDecoration = "line-through";
+    }
+
+    if (todo.task.length < 1) {
+        return alert("please write something in the box");
+    }
+    li.innerText = todo.task;
+    li.style.color = "white"
+    li.appendChild(tickIcon);
+    li.appendChild(binIcon);
+    return li;
+}
+
+function appendTodo(todo) {
+    let list = document.getElementById("todo-list");
+    const li = createDOMListItem(todo);
+    list.appendChild(li);
+}
 
 populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+    // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+    event.preventDefault();
+    const { value: task } = document.querySelector("#todoInput");
+    appendTodo({ task, completed: false });
+    document.querySelector("#todoInput").value = ""
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
-  // Write your code here...
+    event.preventDefault();
+    const list = document.querySelector("ol");
+    const completedTodos = [...document.querySelectorAll("li")].filter((todo) => {
+        return todo.style.textDecoration === "line-through";
+    });
+
+    completedTodos.forEach((todo) => {
+        list.removeChild(todo);
+    });
 }
+const button = document.querySelector(".btn");
+button.addEventListener("click", addNewTodo);
+
+
+document.querySelector("#remove-all-completed").addEventListener("click", deleteAllCompletedTodos);
--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,35 +1,75 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+let triesOutput = document.querySelector(".Tries-output");
+let finalOutput = document.querySelector(".final-output");
+let triesNum = 5;
+triesOutput.innerText = `number of tries : ${triesNum}`;
 
 function guessNumber() {
-  //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
+    //Collect input from the user
 
-  //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+    let guess = Number(document.querySelector(".inputs-Values").value);
+    if (guess < 1 || guess > 100 || guess === "" || !guess) {
+        finalOutput.innerText = "Please enter a number between 1 and 100";
+    } else if (guess === randomNumber) {
+        finalOutput.textContent = `${guess} "is correct, You Win!`;
+        finalOutput.style.color = "green"
+        triesOutput.innerText = `It took you ${triesNum} tries`;
+        //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
+    } else if (guess > randomNumber) {
+        //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
+        if (triesNum > 1) {
+            finalOutput.textContent = "Numbers is too high, try again";
+            finalOutput.style.color = "orange"
+            triesNum--;
+            triesOutput.innerText = `number of tries : ${triesNum}`;
+        }
+        //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+    } else {
+        triesOutput.innerText = `You Lose, the number was ${randomNumber}`;
+        triesOutput.style.color = "red"
+        finalOutput.innerText = "";
+    }
+} else if (guess < randomNumber) {
+    if (triesNum > 1) {
+        finalOutput.textContent = "Numbers is too low, try again";
+        finalOutput.style.color = "blue"
+        triesNum--;
+        triesOutput.innerText = `number of tries : ${triesNum}`;
+    } else {
+        triesOutput.innerText = `You Lose, the number was ${randomNumber}`;
+        triesOutput.style.color = "red"
+        finalOutput.innerText = "";
 
-  //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
-
-  //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
-
-  //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+    }
 }
+
 
 // For this task we will be making a "New Game" button function which will reset our game,
 // Once the user clicks on this button the user will have new random number to guess
 // 1. Reset the values inside the body of the function
 // 2. Attach our new game button using an event listener to the .btnNewGame button
 function newGame() {
-  //Your code here
-  //Reset randomNumber
-  //Reset users input field
-  //Reset tries, and triesTaken by the user
+    //Your code here
+    //Reset randomNumber
+    //Reset users input field
+    //Reset tries, and triesTaken by the user
 }
-
-//keyboard exception
+randomNumber = Math.floor(Math.random() * 100 + 1);
+document.querySelector(".inputs-Values").value = "";
+triesNum = 5;
+finalOutput.innerHTML = "Guess a number between 1 and 100";
+triesOutput.innerHTML = `Number of Tries: ${triesNum}`;
+finalOutput.style.color = "black"
+triesOutput.style.color = "black"
+    //keyboard exception
 function keyBoardEvents(e) {
-  if (e.keyCode === 13) {
-    guessNumber();
-  }
+    if (e.keyCode === 13) {
+        guessNumber();
+    }
 }
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
 document.addEventListener("keypress", keyBoardEvents);
+
+let NewGameBtn = document.querySelector(".btnNewGame");
+NewGameBtn.addEventListener("click", newGame);
