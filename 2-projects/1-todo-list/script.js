d77f07d5969cbe2d925aced028dce081b9e23a76
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-01-21 10:47:27 +0000

    Project Done

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,5 +1,21 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
+  list.innerHTML = "";
+  todos.forEach((todo, idx) => {
+    const todoItem = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      delete todos[idx];
+      populateTodoList(todos);
+    });
+    const li = document.createElement("li");
+    li.innerText = todo.task;
+    li.style.color = "red";
+    todoItem.appendChild(li);
+    todoItem.appendChild(deleteButton);
+    list.appendChild(todoItem);
+  });
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
 
@@ -14,6 +30,13 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
+  const inputValue = document.getElementById("todoInput");
+  const newTodoItem = {
+    task: inputValue.value,
+    completed: false,
+  };
+  todos.push(newTodoItem);
+  populateTodoList(todos);
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
