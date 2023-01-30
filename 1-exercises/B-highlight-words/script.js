f124958163ceebfbb61fe087bff3a8429ad4e4c5
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-01-30 18:58:40 +0000

    Exercise 1 -B - done

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,5 +1,31 @@
 function highlightWords(paragraph, colours) {
-  // Write your code here...
+  //adding paragraph
+  let content = document.getElementById("content");
+  let text = document.createElement("p");
+  content.appendChild(text);
+
+  //select menu
+  let select = document.createElement("select");
+  content.appendChild(select);
+  colours.forEach((colour) => {
+    let option = document.createElement("option");
+    option.text = colour;
+    select.add(option);
+  });
+
+  //looping through paragraph
+  let textArray = paragraph.split(" ");
+  textArray.forEach((word) => {
+    let span = document.createElement("span");
+    span.innerText = word + " ";
+    text.appendChild(span);
+    span.addEventListener("click", () => {
+      let colour = document.querySelector("select").value;
+      if (colour != "none") {
+        span.style.backgroundColor = colour;
+      }
+    });
+  });
 }
 
 const paragraph =
@@ -8,3 +34,14 @@ const paragraph =
 const colours = ["yellow", "green", "blue", "none"];
 
 highlightWords(paragraph, colours);
+
+// - Create a `<p>` element.
+// - Create a `<select>` element.
+// - Iterate over the options array and create an `<option>` element in the `<select>` for each element.
+// - You'll need to turn the paragraph into an array to iterate over it. You can use the `.split(" ")` method here.
+// - Iterate over the array of words.
+// - For each word, create a `<span>` element and set the innerText to the word, plus a space - " ". Add this to the `<p>`.
+// - Each `<span>` should have an eventListener that will listen for clicks.
+// - When clicked, we need to check the value of the `<select>` element using the `.value` property.
+// - We can then update the `background-color` property of the `<span>` with the value of the select - remember that the value "none" is a special case and we need to be handled differently.
+// - All of your HTML should go inside the `<div>` with the id **"content"**.
