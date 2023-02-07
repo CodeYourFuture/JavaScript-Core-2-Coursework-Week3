095b82a55778eca14ea5c1fa598d49cf3c250ccf
Gayle Ngozi <gaylengozi@gmail.com>
2023-02-07 11:01:40 +0000

    todo list project updated

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,5 +1,6 @@
 function populateTodoList(todos) {
   let list = document.getElementsByTagName("ul")[0];
+  list.innerHTML = "";
   let listClass = document.querySelectorAll(".list-group");
   listClass[0].style.display = "flex";
   listClass[0].style.flexDirection = "column";
@@ -36,23 +37,7 @@ function populateTodoList(todos) {
     item.style.border = "1px solid rgba(0, 0, 0, .125)";
   }
 }
-// let list = document.getElementById("#todo-list");
-// for (let chore of todos) {
-//   const listOfJobs = document.createElement("li");
-//   listOfJobs.innerText = chore.task;
 
-//   list.appendChild(listOfJobs);
-
-//   const span = document.createElement("span");
-//   span.className = "badge bg-primary rounded-pill";
-
-//   const check = document.createElement("i");
-//   check.className = "fa fa-check";
-//   check.setAttribute("aria-hidden", "true");
-
-//   const trash = document.createElement("i");
-//   trash.className = "fa fa-trash";
-//   trash.setAttribute("aria-hidden", "true");
 
 // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 
@@ -67,29 +52,18 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
+  event.preventDefault();
+  const inputText = document.querySelector("#todoInput").value;
+  todos.push({task: inputText, completed: false})
+  populateTodoList(todos);
+  document.querySelector("#todoInput").value = "";
+} 
+
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   // event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 
-  let todo = document.querySelector(",btn");
-  todo.setAttribute("id", "addTodo");
-  event.preventDefault();
 
-  let addTodo = document.querySelector("#addTodo");
-  let list = document.querySelector("#todo-list");
-  const listOfJobs = document.createElement("li");
-
-  addTodo.addEventListener("click", (event) => {
-    event.preventDefault();
-    const textOfJobs = document.querySelector("#writeTask").value;
-    list.appendChild(listOfJobs);
-
-    listOfJobs.innerText = textOfJobs;
-    const span = document.createElement("span");
-    const check = document.createElement("i");
-    const trash = document.createElement("i");
-  });
-}
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
