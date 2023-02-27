71b3936bae14d34bf310329cb5d2c56ce035d1ca
Karla Grajales <k.grajalesluna@gmail.com>
2023-02-27 16:28:57 +0000

    A-reading-list done

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,6 +1,32 @@
+
 function readingList(books) {
-  // Write your code here...
+  let content = document.getElementById("content");
+  let listEl = document.createElement("ul")
+  content.appendChild(listEl)
+  books.forEach((book) => {
+    let newList = document.createElement("li");
+    newList.className = "container";
+    let titleBook = document.createElement("p");
+    titleBook.innerText =` ${book.title} \n ${book.author}`;
+    titleBook.className = "title";
+    newList.appendChild(titleBook);
+    let imgOfBooks = document.createElement("img")
+    imgOfBooks.className = "picture"
+    imgOfBooks.src = book.bookCoverImage
+    newList.appendChild(imgOfBooks);
+    if (book.alreadyRead === true) {
+      newList.className = "grenn";
+    } else {
+      newList.className = "red";
+    }
+  listEl.appendChild(newList);
+
+  });
+
 }
+
+
+
 
 // for the tests, do not modify this array of books
 const books = [
--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,3 +1,5 @@
+const { check } = require("prettier");
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
@@ -10,7 +12,33 @@ let todos = [
   { task: "Do the shopping", completed: false },
 ];
 
-populateTodoList(todos);
+//show the list element 
+function populateTodoList(todos){
+  let list = document.createElementById("todo-list");
+  //create a new  li
+  const task = document.createElemtent('li');
+  task.innerText = todos[0].task; 'list-group-item d-flex justify-content-between align-items-clearInterval';
+  task.className = 'list-group-item d-flex'
+
+  const span = documento.createElementById('span');
+  span.className = 'badge bg-primary rounded-pill';
+
+  const check = documento.createElementById('i');
+  span.className = 'fa fa-check';
+  check.setAtribute('aria-hidden', 'true')
+
+  const trash = documento.createElementById('i');
+  span.className = 'fa fa-trash';
+  check.setAtribute('aria-hidden', 'true')
+
+  span.appendiChild(check);
+  span.appendiChild(trash);
+
+  task.appendChild(span)
+
+  //show the list element  as a child of the todo list
+  list.appendChild(task);
+}
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
