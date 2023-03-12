d53378b70e0dae5e2d4a8adb54e2d14b3f799079
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-03-12 04:51:48 +0000

    refactoring

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,22 +1,24 @@
 function highlightWords(paragraph, colours) {
   // Write your code here...\
   const div = document.getElementById('content');
-  let eachWord = '';
   const arrOfWords = paragraph.split(" ");
-  arrOfWords.forEach((word, index) => {
-    eachWord += `<span id="${index}">${word}</span> `;
+  let eachWord = '';
+ 
+  arrOfWords.forEach((word) => {
+    eachWord += `<span>${word}</span> `;
   });
-  let eachOption = '';
-  colours.forEach(colour => {
-    eachOption += `<option value="${colour}">${colour}</option>`
-  })
+
+  let eachOption = colours.map((color) => {
+    return `<option value="${color}">${color}</option>`
+  });
+
   let result = "";
   result += `
-  <p>${eachWord}</p>
-  <select id="mySelect">
-  <option value="1">--Please choose an option--</option>
-  ${eachOption}
-  </select>`
+      <p>${eachWord}</p>
+      <select id="mySelect">
+      <option value="">--Please choose an option--</option>
+      ${eachOption}
+      </select>`
   div.innerHTML = result;
 }
 
@@ -28,20 +30,13 @@ const colours = ["yellow", "green", "blue", "none"];
 highlightWords(paragraph, colours);
 
 const selectSpanEvent = document.querySelector('p');
-const selectElement = document.getElementById("mySelect");
-
-let currentColor = "";
+const currentColor = document.getElementById("mySelect");
 
 selectSpanEvent.addEventListener('click', (event) => {
-    createSelectOption(event.target.id);
+  if (event.target.tagName === 'SPAN') {
+    createSelectOption(event.target);
+  }
 })
-
-selectElement.addEventListener("change", function() {
-  currentColor = selectElement.value;
-});
-
-function createSelectOption(spanId) {
-  console.log(spanId)
-  const span = document.getElementById(`${spanId}`)
-  span.style.backgroundColor = currentColor;
+function createSelectOption(span) {
+  span.style.backgroundColor = currentColor.value;
 }
