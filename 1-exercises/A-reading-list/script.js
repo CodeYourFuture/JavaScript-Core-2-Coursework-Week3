3387c6c67aaaf6d654ccc2db4aa115eeba8d1887
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-03-30 14:23:24 +0100

    Exercise - A

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,23 @@
 function readingList(books) {
   // Write your code here...
+  const result = document.getElementById("reading-list");
+  for (let i = 0; i < books.length; i++) {
+    let book = books[i];
+    const newLi = document.createElement("li");
+    const addParag = document.createElement("p");
+    addParag.textContent = `${book.title} by ${book.author}`;
+    const addImg = document.createElement("img");
+    addImg.src = book.bookCoverImage;
+    newLi.appendChild(addParag);
+    newLi.appendChild(addImg);
+    if (book.alreadyRead) {
+      newLi.style.backgroundColor = "green";
+    } else {
+      newLi.style.backgroundColor = "red";
+    }
+
+    result.appendChild(newLi);
+  }
 }
 
 // for the tests, do not modify this array of books
