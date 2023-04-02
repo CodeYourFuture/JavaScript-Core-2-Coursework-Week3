c8ff441bcd1a313f7205f5c2d9bf598cd9377bcb
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-04-02 22:04:33 +0100

    exercises done

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,37 @@
+
 function readingList(books) {
-  // Write your code here...
+  const bookDiv = document.querySelector("#content");
+  const listOfBooks = document.createElement('ul');
+  bookDiv.appendChild(listOfBooks);
+  for (let book of books){
+    let bookTitle = document.createElement("p");
+    bookTitle.innerText = `${book.title} by ${book.author}`;
+
+    let bookImg = document.createElement("img");
+    bookImg.src = book.bookCoverImage;
+
+    let liElement = document.createElement("li");
+    liElement.appendChild(bookTitle);
+    liElement.appendChild(bookImg);
+
+    listOfBooks.appendChild(liElement);
+    if (book.alreadyRead){
+      liElement.style.backgroundColor = "green";
+    } else {
+      liElement.style.backgroundColor = "red";
+    }
+
+  }
+bookDiv.appendChild(listOfBooks);
+
+//  Iterate through the list of books.
+// + For each book, create a `<p>` element with the book title and author.
+// + Add an `<img>` to each book that links to a URL of the book cover.
+// + Use a `<ul>` and `<li>` to display the books and append it to the page.
+// + Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
+// + All of your HTML should go inside the `<div>` with the id **"content"**.
+
+// The end result should look something like this
 }
 
 // for the tests, do not modify this array of books
--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,5 +1,42 @@
+// + Create a `<p>` element.
+// + Create a `<select>` element.
+// + Iterate over the options array and create an `<option>` element in the `<select>` for each element.
+// + You'll need to turn the paragraph into an array to iterate over it. You can use the `.split(" ")` method here.
+// + Iterate over the array of words.
+// + For each word, create a `<span>` element and set the innerText to the word, plus a space - " ". Add this to the `<p>`.
+// + Each `<span>` should have an eventListener that will listen for clicks.
+// - When clicked, we need to check the value of the `<select>` element using the `.value` property.
+// - We can then update the `background-color` property of the `<span>` with the value of the select - remember that the value "none" is a special case and we need to be handled differently.
+// + All of your HTML should go inside the `<div>` with the id **"content"**.
+
+
 function highlightWords(paragraph, colours) {
-  // Write your code here...
+  let paragraf = document.createElement("p");
+  let selectElement = document.createElement("select");
+  for (let colour of colours){
+    let colorOption = document.createElement("option");
+    colorOption.value = colour;
+    colorOption.innerText = colour;
+    selectElement.appendChild(colorOption);
+  }
+
+  let wordsArray = paragraph.split(" ");
+  for (let word of wordsArray){
+    let spanElement = document.createElement("span");
+    spanElement.innerText = word + " ";
+    paragraf.appendChild(spanElement);
+
+    spanElement.addEventListener("click", function (event){
+      event.preventDefault();
+      if (selectElement.value === "none"){
+        spanElement.style.backgroundColor = "white";
+      } else {
+        spanElement.style.backgroundColor = selectElement.value;
+      }
+    })
+  }
+  document.querySelector("#content").appendChild(paragraf);
+  document.querySelector("#content").appendChild(selectElement);
 }
 
 const paragraph =
