ac4933546bbc345fb4dd71074a3ee4d2a02fbecb
siveromar <109865820+siveromar@users.noreply.github.com>
2023-04-04 02:48:12 +0100

    js.week4

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,15 @@
 function readingList(books) {
   // Write your code here...
+  /*Iterate through the list of books.
+  - For each book, create a `<p>` element with the book title and author.
+  - Add an `<img>` to each book that links to a URL of the book cover.
+  - Use a `<ul>` and `<li>` to display the books and append it to the page.
+  - Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
+  - All of your HTML should go inside the `<div>` with the id **"content"**.
+  
+  The end result should look something like this
+  
+  ![reading-list](reading-list.png) **/
 }
 
 // for the tests, do not modify this array of books
