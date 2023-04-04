524975c9f1bf2d448004fce067508292d872fcbd
Olha Danylevska <odanilevskaya2@gmail.com>
2023-04-04 22:02:54 +0100

    1-exercises / A-reading-list (finished)

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,6 +1,43 @@
+let content = document.querySelector("#content")
+let takeUL = document.querySelector("#reading-list")
+
+
 function readingList(books) {
-  // Write your code here...
+  
+  books.forEach((book) => {
+    let listElement = document.createElement("li")
+
+    let createP = document.createElement("p") 
+    createP.innerText = `${book.title} , ${book.author}`
+
+    let createImage = document.createElement("img")
+    createImage.src = book.bookCoverImage 
+    createImage.classList.add("image")
+
+    
+    listElement.appendChild(createP)
+    listElement.appendChild(createImage)
+    takeUL.appendChild(listElement)
+    content.appendChild(takeUL)
+    
+    if (book.alreadyRead == true){
+      listElement.classList.add("greenMark")
+    } else {
+      listElement.classList.add("redMark")
+      }
+  });
+  
 }
+
+
+
+
+
+
+
+
+
+
 
 // for the tests, do not modify this array of books
 const books = [
@@ -26,3 +63,5 @@ const books = [
 ];
 
 readingList(books);
+
+
