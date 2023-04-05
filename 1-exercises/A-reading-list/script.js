d0c7f6e90aac3549943de79ecd978d3736e7b236
mandy-cheung <109825538+m4ndycheung@users.noreply.github.com>
2023-04-05 15:37:11 +0100

    Added to 1-exercises

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,38 @@
 function readingList(books) {
-  // Write your code here...
+  //   - For each book, create a `<p>` element with the book title and author.
+  for (const book of books) {
+    const pElement = document.createElement("p");
+
+    // we want to assign something inside the p
+    pElement.innerText = `${book.title} by ${book.author}`;
+
+    //   - Add an `<img>` to each book that links to a URL of the book cover.
+    const bookImage = document.createElement("img");
+    bookImage.src = `${book.bookCoverImage}`;
+
+    //   - Use a `<ul>` and `<li>` to display the books and append it to the page.
+
+    const bookList = document.querySelector("#reading-list");
+    const bookListItem = document.createElement("li");
+
+    bookList.appendChild(bookListItem);
+
+    // Appending the pElement and the image to each <li>
+    bookListItem.appendChild(pElement).appendChild(bookImage);
+
+    //   - Change the style of the book depending on whether you have read it (green) or not (red). Hint: try adding some CSS classes to styles.css for these styles.
+
+    // ***How can we change the colours of each list item?
+    // This works, why does it work - ask Michael!
+    if (book.alreadyRead === false) {
+      bookListItem.classList.add("red");
+    } else {
+      bookListItem.classList.add("green");
+    }
+  }
+
+  // ***How can we change the colours of each list item?
+  // const allListItems = document.querySelectorAll("#reading-list > *");
 }
 
 // for the tests, do not modify this array of books
