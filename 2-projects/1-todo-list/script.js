d3fc37a344bbdff18a526b2e3cf4dae71ab54c4b
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-04-07 22:43:23 +0100

    updated project todo-list

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,9 +1,27 @@
+function strikeCompletedTask() {
+  
+}
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
   todos.map((todo) => {
     let cardContainer = document.createElement("div");
     let newListItem = document.createElement("li");
+    let iconSpan = document.createElement("span");
+    let checkIcon = document.createElement("i");
+    let trashIcon = document.createElement("i");
+    checkIcon.classList.add("fa", "fa-check");
+    checkIcon.addEventListener("click", function() {
+      if (cardContainer.classList.contains("strike") === true) {
+        cardContainer.classList.remove("strike");
+      } else {
+        cardContainer.classList.add("strike");
+      }      
+    })
+    trashIcon.classList.add("fa", "fa-trash");
+    iconSpan.classList.add("rounded-pill", "bg-primary");
+    iconSpan.append(checkIcon, trashIcon);
     // let text = document.createTextNode(todo.task);
     // newListItem.appendChild(text);
     cardContainer.classList.add(
@@ -13,7 +31,7 @@ function populateTodoList(todos) {
       "align-items-center"
     );
     newListItem.innerText = todo.task;
-    cardContainer.appendChild(newListItem);
+    cardContainer.append(newListItem, iconSpan);
     list.appendChild(cardContainer);
   });
 }
@@ -32,9 +50,28 @@ function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let list = document.getElementById("todo-list");
+  let cardContainer = document.createElement("div");
+  let todoText = document.querySelector("#todoInput").value;
+  let newListItem = document.createElement("li");
+  cardContainer.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+    );
+    newListItem.append(todoText);
+    cardContainer.appendChild(newListItem);
+    list.appendChild(cardContainer);
+    document.querySelector("#todoInput").value = "";
+    
 }
+
+let addButton = document.querySelector("#add-todo");
+addButton.addEventListener("click", addNewTodo);
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
+  let resetButton = document.querySelector("#remove-all-complete")
 }
