6c1c10bfb56670e08e0b77a646f822207b0302a8
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-04-10 14:38:25 +0100

    all done

--- 1-exercises/B-highlight-words/script.js
+++ 1-exercises/B-highlight-words/script.js
@@ -1,18 +1,19 @@
 let select = document.createElement("select");
 content.appendChild(select);
-
 const selectContainer = document.querySelector("select");
+selectContainer.innerHTML = `<option value="Please choose a colour"> Please choose a color</option>`;
+
 
 function highlightWords(paragraph, colours) {
   let P = document.createElement("p");
   content.appendChild(P);
   let arr = paragraph.split(" ");
-  console.log(arr);
+  
   for (let word of arr) {
-    P.innerHTML += `<span>${word}</span>&nbsp`;
+    P.innerHTML += `<span>${word} </span>&nbsp`;
   }
   colours.forEach((el) => {
-    selectContainer.innerHTML += `<option value="${el}">${el}</option>`;
+    selectContainer.innerHTML += `<option value="${el.toLowerCase()}">${el}</option>`;
   });
 }
 
@@ -35,6 +36,6 @@ window.addEventListener("click", (event) => {
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
 
-const colours = ["yellow", "green", "blue", "none"];
+const colours = ["Yellow", "Green", "Blue", "None"];
 
 highlightWords(paragraph, colours);
--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,6 +1,32 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  for (let todo of todos) {
+    const li = document.createElement("li");
+    li.innerHTML = todo.task;
+    list.appendChild(li);
+
+    const checkBtn = document.createElement("button");
+    checkBtn.innerHTML = '<i class = "fa fa-check"></i>';
+    li.appendChild(checkBtn);
+
+    checkBtn.addEventListener("click", () => {
+      if (li.style.textDecoration === "line-through") {
+        li.style.textDecoration = "";
+      } else {
+        li.style.textDecoration = "line-through";
+      }
+      todo.completed = !todo.completed;
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class = "fa fa-trash"></i>';
+    li.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", () => {
+      list.removeChild(li);
+    });
+  }
 }
 
 // These are the same todos that currently display in the HTML
@@ -13,13 +39,33 @@ let todos = [
 populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
+const addBtn = document.getElementsByClassName("btn btn-primary mb-3")[0];
+addBtn.addEventListener("click", addNewTodo);
+
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+  let input = document.getElementById("todoInput");
+  if (input.value !== "") {
+    populateTodoList([{ task: input.value, completed: false }]);
+    input.value = "";
+  } else return;
+
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  
 }
+
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
+  let list = document.getElementById("todo-list");
+  const lis = document.querySelectorAll("li");
+  for (let l of lis) {
+    if (l.style.textDecoration === "line-through") {
+      list.removeChild(l);
+    }
+  }
 }
+const delBtnComp = document.getElementById("remove-all-completed");
+delBtnComp.addEventListener("click", deleteAllCompletedTodos);
