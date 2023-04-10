9e858832a919dcf23e17118de98434ac5bad5e78
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-04-10 23:42:40 +0100

    A-reading-list done
    task done

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,24 @@
 function readingList(books) {
-  // Write your code here...
+  for (i = 0; i <= books.length; i++) {
+    let liElement = document.createElement("li");
+    let pElement = document.createElement("p");
+    let bookInfo = books[i].title + " by " + books[i].author;
+    pElement.textContent = bookInfo; // textContent only targets the text portion but leave any HTML alone, but if you go after html it will completely ruin HTML that you had before.
+    if (books[i].alreadyRead === true) {
+      liElement.classList.add("green");
+    } else {
+      liElement.classList.add("red");
+    }
+    liElement.appendChild(pElement);
+    let imgElement = document.createElement("img");
+    let bookImageUrl = books[i].bookCoverImage;
+    imgElement.src = bookImageUrl;
+    liElement.appendChild(imgElement);
+    let ulElement = document.querySelector("#reading-list");
+    ulElement.appendChild(liElement);
+    let divElement = document.querySelector("#content");
+    divElement.appendChild(ulElement);
+}
 }
 
 // for the tests, do not modify this array of books
