3360a8319e6837f4e2393603296834157de53399
Saliha Popal <salihapopal@gmail.com>
2023-04-18 22:31:14 +0100

    done.

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -19,7 +19,7 @@ function populateTodoList(toDos) {
       } else {
         liEle.style.textDecoration = "line-through";
       }
-      todo.completed = !todo.completed;
+        todo.completed =!todo.completed;
     });
 
     const deleteBtn = document.createElement("button");
@@ -53,11 +53,12 @@ function addNewTodo(event) {
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank aft er creating a todo!
   let inputElement = document.getElementById("todoInput");
-  let ulFlusher = document.getElementById("todo-list");
-    ulFlusher.innerHTML = "";
+  
   if (inputElement.value === "") {
     
   } else {
+ let ulFlusher = document.getElementById("todo-list");
+    ulFlusher.innerHTML = "";
     let newItem = {};
     newItem["task"] = inputElement.value;
     newItem["completed"] = false;
@@ -66,30 +67,18 @@ function addNewTodo(event) {
   }
 }
 
-
-
 // Advanced challenge: Write a function that checks the toDos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-// function deleteAllCompletedToDos(event) {
-//   // Write your code here...
-//   event.preventDefault();
-//   toDos = toDos.filter(todo => !todo.completed);
-//   let listEle = document.getElementById("todo-list");
-//   // const completedToDos = document.querySelectorAll("liEle.completed");
-//   document.querySelectorAll("listEle.completed");
-//   completedToDos.forEach(function(){
-//     listEle.removeChild()});
-
-//   const liEle = document.querySelectorAll("li");
-//   // let child_array = Array.from(liEle.child);
-//   for (let li of liEle.child) {
-//     if (li.style.textDecoration === "line-through") {
-//       listEle.removeChild(li);
-//     }
-//   }
-// }
-// const delBtnComp = document.querySelectorAll("remove-all-completed");
-
-
-// delBtnComp.addEventListener("click", deleteAllCompletedToDos);
-
+function deleteAllCompletedToDos(event) {
+  // Write your code here...
+  let listEle = document.getElementById("todo-list");
+  const liEle = document.querySelectorAll("li");
+  // let child_array = Array.from(liEle.child);
+  for (let li of liEle) {
+    if (li.style.textDecoration === "line-through") {
+      listEle.removeChild(li);
+    }
+  }
+}
+const delBtnComp = document.getElementById("remove-all-completed");
+delBtnComp.addEventListener("click", deleteAllCompletedToDos);
 
