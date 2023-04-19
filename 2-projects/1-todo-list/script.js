eb51f8ef41dc88ab618e7b4ba4d061bfca34c502
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-04-19 23:52:08 +0100

    Update js

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -29,8 +29,7 @@ function populateTodoList(todos) {
 
 }
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
+
 let todos = [
   { task: "Complete everything", completed: false },
   { task: "Conquer Earth", completed: false },
@@ -38,24 +37,22 @@ let todos = [
 
 populateTodoList(todos);
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-    const todoInput = document.querySelector('#todoInput');
-    const newTask = todoInput.value.trim();
+    let todoInput = document.querySelector('todoInput');
+    let newTask = todoInput.value.trim();
     
     if (newTask !== "") {
       const todo = { task: newTask, completed: false };
       todos.push(todo);
       populateTodoList(todos);
       todoInput.value = "";
+      populateTodoList(todos);
     }
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  //event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+
 }
 const addBtn = document.querySelector('btn-primary');
 addBtn.addEventListener("click", addNewTodo);
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
+
 function deleteAllCompletedTodos() {
   todos.forEach((todo, index) => {
     const li = document.querySelectorAll('list-group')[index];
@@ -67,6 +64,6 @@ function deleteAllCompletedTodos() {
   });
 }
 
-const removeAllCompletedBtn = document.querySelector('#remove-all-completed');
-removeAllCompletedBtn.addEventListener("click", deleteAllCompletedTodos);
+let addToDoButton = document.querySelector("addButton");
+addToDoButton.addEventListener("click", addNewTodo);
 
