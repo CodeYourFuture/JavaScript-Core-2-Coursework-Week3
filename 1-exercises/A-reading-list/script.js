9f530e5c6df16100a05e5e424adcb50dfe89f5e6
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-05-03 18:00:39 +0100

    done-ex:1

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,5 +1,27 @@
 function readingList(books) {
-  // Write your code here...
+  for (let book of books) {
+    let divEl = document.getElementById("content");
+    let pEl = document.createElement("p");
+    divEl.appendChild(pEl);
+
+    let ulEl = document.createElement("ul");
+    pEl.appendChild(ulEl);
+
+    let liEl = document.createElement("li");
+    ulEl.appendChild(liEl);
+    liEl.innerHTML = `${book.title} by ${book.author}.`;
+
+    let imageEl = document.createElement("img");
+    imageEl.src = book.bookCoverImage;
+    imageEl.style.height = "10rem";
+    ulEl.appendChild(imageEl);
+
+    if (book.alreadyRead) {
+      pEl.className += "read-book";
+    } else {
+      pEl.className += "unread-book";
+    }
+  }
 }
 
 // for the tests, do not modify this array of books
