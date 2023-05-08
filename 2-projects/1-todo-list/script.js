4a8a4fbb3cb7df40da0405a13cb12bd04248c0b0
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-05-08 05:53:23 +0300

    Updated scripts

--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,68 +1,76 @@
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  list.innerHTML = ""; //Clear existing todos in the list 
+  const todoList = document.getElementById("todo-list");
+  for (const todo of todos) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = todo.task;
+    todoList.appendChild(listItem);
 
-  todos.forEach((todo, index) => {
-    let li = document.createElement("li");
-    li.className = "list-group-item";
-    li.innerText = todo.task;
+    const checkBtn = document.createElement("button");
+    checkBtn.classList.add("badge", "bg-primary", "rounded-pill");
+    const tickElement = document.createElement("i");
+    tickElement.classList.add("fa", "fa-check");
+    checkBtn.appendChild(tickElement);
+    listItem.appendChild(checkBtn);
 
-    let completeBtn = document.createElement("button");
-    completeBtn.className = "btn btn-outline-success float-end";
-    completeBtn.innerText = "Complete";
-    completeBtn.addEventListener("click", () => {
-      li.classList.toggle("text-decoration-line-through");
+    checkBtn.addEventListener("click", () => {
+      if (listItem.style.textDecoration === "line-through") {
+        listItem.style.textDecoration = "";
+      } else {
+        listItem.style.textDecoration = "line-through";
+      }
       todo.completed = !todo.completed;
     });
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.className = "btn btn-outline-danger float-end me-2";
-    deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("badge", "bg-primary", "rounded-pill");
+    const binElement = document.createElement("i");
+    binElement.classList.add("fa", "fa-trash");
+    deleteBtn.appendChild(binElement);
+    listItem.appendChild(deleteBtn);
+
     deleteBtn.addEventListener("click", () => {
-      todos.splice(index, 1);
-      populateTodoList(todos);
+      todoList.removeChild(listItem);
     });
-
-    li.appendChild(completeBtn);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-  });
-}
-
-
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [];
-
-populateTodoList(todos);
-
-let addToDoButton = document.getElementById("addButton");
-addToDoButton.addEventListener("click", addNewTodo);
-
-
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  let inputFiled = document.getElementById("todoInput");
-  let inputValue = inputFiled.value.trim();
-
-  if (inputValue !== "") {
-    let addNewTodo = { task: inputValue, completed: false };
-    todos.push(addNewTodo);
-    inputFiled.value = "";
-    populateTodoList(todos);
   }
 }
 
-// Advanced challenge: Write a function that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  for (let i = todos.length - 1; i >= 0; i--) {
-    let todo = todos[i];
-    let li = document.querySelectorAll(".list-group-item")[i];
-    if (todo.completed) {
-      todos.splice(i, 1);
-      li.remove();
+const initialTodos = [
+  { task: "Wash the dishes", completed: false },
+  { task: "Do the shopping", completed: false },
+];
+
+populateTodoList(initialTodos);
+
+function addNewTodo(event) {
+  event.preventDefault();
+  const input = document.getElementById("todoInput");
+  if (input.value === "") {
+    return;
+  } else {
+    const newTodo = {
+      task: input.value,
+      completed: false,
+    };
+    initialTodos.push(newTodo);
+    input.value = "";
+    populateTodoList(initialTodos);
+  }
+}
+
+const addBtn = document.querySelector(".btn.btn-primary.mb-3");
+addBtn.addEventListener("click", addNewTodo);
+
+function deleteCompletedTodos() {
+  const todoList = document.getElementById("todo-list");
+  const listItems = document.querySelectorAll("li");
+  for (const listItem of listItems) {
+    if (listItem.style.textDecoration === "line-through") {
+      const todoIndex = Array.from(todoList.children).indexOf(listItem);
+      initialTodos.splice(todoIndex, 1);
+      todoList.removeChild(listItem);
     }
   }
 }
+
+const delBtnComp = document.getElementById("remove-all-completed");
+delBtnComp.addEventListener("click", deleteCompletedTodos);
